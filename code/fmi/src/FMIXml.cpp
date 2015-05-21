da15c86766c07351d3af39c596e596d8a846af3c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-05-21 14:28:04 +0200

    [FMI] Created types to mirror FMI's XML

---
+++ code/fmi/src/FMIXml.cpp
@@ -0,0 +1,91 @@
+/*
+ * parse_fmi_xml.cpp
+ *
+ *  Created on: May 21, 2015
+ *      Author: cady
+ */
+
+#include "FMIXml.hpp"
+
+fmi::DateTime::DateTime() : year(), month(), day(), hours(), minutes(), seconds()
+{
+}
+
+fmi::Attributes::Attributes() :
+    fmiVersion(),
+    modelName(),
+    modelidentifier(),
+    guid(),
+    description(),
+    author(),
+    version(),
+    generationTool(),
+    generationDateAndTime(),
+    variableNamingConvention(),
+    numberOfEventsIndicators()
+{
+}
+
+fmi::DisplayUnitDefinition::DisplayUnitDefinition() :
+        displayUnit(),
+        gain(),
+        offset()
+{
+}
+
+fmi::BaseUnit::BaseUnit():
+        unit(),
+        definitions()
+{
+}
+
+fmi::DefaultExperiment::DefaultExperiment():
+        startTime(),
+        stopTime(),
+        tolerance()
+{
+}
+
+fmi::Annotation::Annotation() :
+        name(),
+        value()
+{
+}
+
+fmi::Tool::Tool() :
+    name(),
+    annotations()
+{}
+
+fmi::RealType::RealType() :
+    quantity(),
+    unit(),
+    displayUnit(),
+    relativeQuantity(),
+    min(),
+    max(),
+    nominal()
+{
+}
+
+fmi::RealAttributes::RealAttributes() :
+        declaredType(),
+        quantity(),
+        unit(),
+        displayUnit(),
+        relativeQuantity(),
+        min(),
+        max(),
+        nominal(),
+        start(),
+        fixed()
+{}
+
+fmi::Xml::Xml() :
+        attributes(),
+        UnitDefinitions(),
+        default_experiment(),
+        vendor_annotations(),
+        type_definitions(),
+        real_model_variables()
+{}
