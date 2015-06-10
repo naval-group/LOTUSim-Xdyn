12ff5f9f8ed40bb1d61f53900c6984d76a53ec82
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-06-10 10:36:11 +0200

    [FMI] Can get working directory
    Shamelessly copied from http://stackoverflow.com/a/145309

--- code/fmi/src/FMI.cpp
+++ code/fmi/src/FMI.cpp
@@ -37,6 +37,30 @@ const char* fmiGetVersion()
     return fmiVersion;
 }
 
+#include <stdio.h>  /* defines FILENAME_MAX */
+#ifdef WINDOWS
+    #include <direct.h>
+    #define GetCurrentDir _getcwd
+#else
+    #include <unistd.h>
+    #define GetCurrentDir getcwd
+ #endif
+
+std::string get_working_directory();
+std::string get_working_directory()
+{
+     char cCurrentPath[FILENAME_MAX];
+
+     if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+     {
+         return "";
+     }
+
+    cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
+
+    return std::string(cCurrentPath);
+}
+
 fmiComponent fmiInstantiateModel (fmiString            instanceName,
                                   fmiString            GUID,
                                   fmiCallbackFunctions functions,
