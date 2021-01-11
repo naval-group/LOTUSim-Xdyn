193dfcd0ba18c11b467ceaad875862bba01d6d2c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-01-11 13:06:15 +0100

    :hammer: Second refactoring proposal

--- code/force_models/inc/HoltropMennenForceModel.hpp
+++ code/force_models/inc/HoltropMennenForceModel.hpp
@@ -6,6 +6,7 @@
 #include <ssc/kinematics.hpp>
 
 #include "ForceModel.hpp"
+#include "EnvironmentAndFrames.hpp"
 
 class HoltropMennenForceModel : public ForceModel
 {
@@ -33,23 +34,60 @@ class HoltropMennenForceModel : public ForceModel
             double app_form_coeff;
             bool apply_on_ship_speed_direction;
         };
+        struct DerivedData
+        {
+                DerivedData(const Input& base_data);
+                double c17;
+                double c15;
+                double c7;
+                double c4;
+                double T;
+                double Pb;
+                double c14;
+                // Dependencies of rank 1
+                double c5;
+                double m3;
+                double Cb;
+                double Cp;
+                double c3;
+                // Dependencies of rank 2
+                double Ca;
+                double S;
+                double c16;
+                double Lr;
+                double lambda;
+                double c2;
+                double iE;
+                // Dependencies of rank 3
+                double c1;
+                double m1;
+                double hull_form_coeff;
+
+            private:
+                DerivedData(); // Deactivated
+        };
+
         HoltropMennenForceModel(const Input& input, const std::string& body_name, const EnvironmentAndFrames& env);
         ssc::kinematics::Wrench operator()(const BodyStates& states, const double t) const override;
         static Input parse(const std::string& yaml);
         static std::string model_name();
 
-        double get_Rf(const BodyStates& states) const;   // Frictional resistance over the hull
-        double get_Rapp(const BodyStates& states) const; // Frictional resistance over the appendages
-        double get_Rw(const BodyStates& states) const;   // Wave-making resistance
-        double get_Rb(const BodyStates& states) const;   // Bulbous bow influence
-        double get_Rtr(const BodyStates& states) const;  // Immersed transom stern influence
-        double get_Ra(const BodyStates& states) const;   // Correlation term between model and full scale
-
-        class Impl;
+        double Rf(const BodyStates& states) const;   // Frictional resistance over the hull
+        double Rapp(const BodyStates& states) const; // Frictional resistance over the appendages
+        double Rw(const BodyStates& states) const;   // Wave-making resistance
+        double Rb(const BodyStates& states) const;   // Bulbous bow influence
+        double Rtr(const BodyStates& states) const;  // Immersed transom stern influence
+        double Ra(const BodyStates& states) const;   // Correlation term between model and full scale
 
     private:
-        const bool apply_on_ship_speed_direction;
-        const TR1(shared_ptr)<Impl> pimpl;
+        const EnvironmentAndFrames env;
+        const double d;
+        const Input input;
+        const DerivedData derived;
+        bool apply_on_ship_speed_direction; // This parameter is used to apply the force on the ship's speed direction rather than the X-axis.
+        const double gravity_force;
+        double Rw_a(const double Fn, const double m4) const;
+        double Rw_b(const double Fn, const double m4) const;
 };
 
 #endif /* FORCE_MODELS_INC_HOLTROPMENNENFORCEMODEL_HPP_ */
