44afd2d5adda1d3db0e9a4be47ce48008f5953b8
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-01-29 21:11:27 +0100

    :pencil2: Renamed template parameter ControllableForceType to ForceType
    Fixed indentation of ForceModel::build_parser() for force models with a
    parser.

--- code/core/inc/ForceModel.hpp
+++ code/core/inc/ForceModel.hpp
@@ -67,45 +67,48 @@ class ForceModel
         std::string get_body_name() const;
         virtual bool is_a_surface_force_model() const;
 
-        template <typename ControllableForceType>
-        static typename boost::enable_if<HasParse<ControllableForceType>, ForceParser>::type build_parser()
+        template<typename ForceType>
+        static typename boost::enable_if<HasParse<ForceType>, ForceParser>::type build_parser()
         {
-            auto parser = [](const YamlModel& yaml, const std::string& body_name, const EnvironmentAndFrames& env) -> boost::optional<ForcePtr>
-                          {
-                              boost::optional<ForcePtr> ret;
-                              if (yaml.model == ControllableForceType::model_name())
-                              {
-                                  std::string context = "Invalid input data for model '" + ControllableForceType::model_name() + "'.";
-                                  try
-                                  {
-                                      ret.reset(ForcePtr(new ControllableForceType(ControllableForceType::parse(yaml.yaml), body_name, env)));
-                                  }
-                                  catch (const InvalidInputException& exception)
-                                  {
-                                      THROW(__PRETTY_FUNCTION__, InvalidInputException, context << std::endl << exception.get_message() << std::endl << "Model containing error is defined line "
-                                              << yaml.index_of_first_line_in_global_yaml << " of the YAML file." << std::endl);
-                                  }
-                                  catch (const YAML::Exception& exception)
-                                  {
-                                      const size_t line_number = yaml.index_of_first_line_in_global_yaml;
-                                      THROW(__PRETTY_FUNCTION__, InvalidInputException, context << std::endl << "Model containing error is defined line "
-                                                                                         << line_number << " of the YAML file." << std::endl << "The error was: " << exception.msg);
-                                  }
-                              }
-                              return ret;
-                          };
+            auto parser = [](
+                const YamlModel& yaml,
+                const std::string& body_name,
+                const EnvironmentAndFrames& env) -> boost::optional<ForcePtr>
+                {
+                    boost::optional<ForcePtr> ret;
+                    if (yaml.model == ForceType::model_name())
+                    {
+                        std::string context = "Invalid input data for model '" + ForceType::model_name() + "'.";
+                        try
+                        {
+                            ret.reset(ForcePtr(new ForceType(ForceType::parse(yaml.yaml), body_name, env)));
+                        }
+                        catch (const InvalidInputException& exception)
+                        {
+                            THROW(__PRETTY_FUNCTION__, InvalidInputException, context << std::endl << exception.get_message() << std::endl << "Model containing error is defined line "
+                                    << yaml.index_of_first_line_in_global_yaml << " of the YAML file." << std::endl);
+                        }
+                        catch (const YAML::Exception& exception)
+                        {
+                            const size_t line_number = yaml.index_of_first_line_in_global_yaml;
+                            THROW(__PRETTY_FUNCTION__, InvalidInputException, context << std::endl << "Model containing error is defined line "
+                                    << line_number << " of the YAML file." << std::endl << "The error was: " << exception.msg);
+                        }
+                    }
+                    return ret;
+                    };
             return parser;
         }
 
-        template <typename ControllableForceType>
-        static typename boost::disable_if<HasParse<ControllableForceType>, ForceParser>::type build_parser()
+        template <typename ForceType>
+        static typename boost::disable_if<HasParse<ForceType>, ForceParser>::type build_parser()
         {
             auto parser = [](const YamlModel& yaml, const std::string& body, const EnvironmentAndFrames& env) -> boost::optional<ForcePtr>
             {
                 boost::optional<ForcePtr> ret;
-                if (yaml.model == ControllableForceType::model_name())
+                if (yaml.model == ForceType::model_name())
                 {
-                    ret.reset(ForcePtr(new ControllableForceType(body, env)));
+                    ret.reset(ForcePtr(new ForceType(body, env)));
                 }
                 return ret;
             };
