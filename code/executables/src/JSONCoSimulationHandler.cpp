270a9b74d39d11f068de2918c3a9dfd33d3d01b5
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-02-08 12:21:45 +0100

    :hammer: Aligned the implementations of JSONCoSimulationHandler::operator() and JSONModelExchangeHandler::operator()

--- code/executables/src/JSONCoSimulationHandler.cpp
+++ code/executables/src/JSONCoSimulationHandler.cpp
@@ -32,8 +32,16 @@ void JSONCoSimulationHandler::operator()(const ssc::websocket::Message& msg)
     const std::function<void(const std::string&)> quiet_error_outputter = [&msg](const std::string& what) {msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
     const std::function<void(const std::string&)> verbose_error_outputter = [&msg](const std::string& what) {std::cerr << current_date_time() << " Error: " << what << std::endl; msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
     const auto error_outputter = verbose ? verbose_error_outputter : quiet_error_outputter;
-    const std::function<void(void)> quiet_f = [&msg, this, &input_json]() {msg.send_text(serialize(this->sim_server->play_one_step(input_json)));};
-    const std::function<void(void)> verbose_f = [&msg, this, &input_json]() {const std::string json = serialize(this->sim_server->play_one_step(input_json)); std::cout << current_date_time() << " Sending: " << json << std::endl; msg.send_text(json);};
-    const std::function<void(void)> f = verbose ? verbose_f : quiet_f;
+    const auto f = [&input_json, this, &msg]()
+        {
+            SimServerInputs server_inputs(deserialize(input_json), sim_server->get_Tmax());
+            const std::vector<YamlState> dx_dt = sim_server->handle(server_inputs);
+            const std::string output_json = serialize(dx_dt);
+            if (verbose)
+            {
+                std::cout << current_date_time() << " Sending: " << output_json << std::endl;
+            }
+            msg.send_text(output_json);
+        };
     report_xdyn_exceptions_to_user(f, error_outputter);
 }
--- code/executables/src/JSONModelExchangeHandler.cpp
+++ code/executables/src/JSONModelExchangeHandler.cpp
@@ -33,10 +33,9 @@ void JSONModelExchangeHandler::operator()(const ssc::websocket::Message& msg)
     const std::function<void(const std::string&)> quiet_error_outputter = [&msg](const std::string& what) {msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
     const std::function<void(const std::string&)> verbose_error_outputter = [&msg](const std::string& what) {std::cerr << current_date_time() << " Error: " << what << std::endl; msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
     const auto error_outputter = verbose ? verbose_error_outputter : quiet_error_outputter;
-    const std::string input_yaml = msg.get_payload();
-    const auto f = [&input_yaml, this, &msg]()
+    const auto f = [&input_json, this, &msg]()
         {
-            SimServerInputs server_inputs(deserialize(input_yaml), sim_server->get_Tmax());
+            SimServerInputs server_inputs(deserialize(input_json), sim_server->get_Tmax());
             const std::vector<double> dx_dt = sim_server->handle(server_inputs);
             const std::string output_json = serialize(dx_dt);
             if (verbose)
