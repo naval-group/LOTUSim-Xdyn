069971cdac8155e58137f7a0c8302de7edae9abe
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-02-18 17:41:29 +0100

    :books: Added a link to the MR discussion explaining why we need the sleep

--- code/executables/src/JSONWebSocketServer.cpp
+++ code/executables/src/JSONWebSocketServer.cpp
@@ -42,6 +42,7 @@ void JSONWebSocketServer<ServiceT>::start(const short unsigned int port, const b
     std::cout << "Starting websocket server on " << ADDRESS << ":" << port << " (press Ctrl+C to terminate)" << std::endl;
     std::shared_ptr<ssc::websocket::Server> w(new ssc::websocket::Server(handler, port, debug));
     signal(SIGINT, stop_simulation);
+    // Cf. https://gitlab.com/sirehna_naval_group/xdyn/-/merge_requests/7#note_511727357 for why we need to do this
     while(!stop){std::this_thread::sleep_for(std::chrono::milliseconds(500));}
     std::cout << std::endl << "Gracefully stopping the websocket server..." << std::endl;
 }
