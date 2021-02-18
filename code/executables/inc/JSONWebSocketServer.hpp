58837a60a49399af413dbefbc86248237e8bcbdc
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-02-18 11:47:36 +0100

    :lipstick: Fixed indentation in JSONWebSocketServer.hpp

--- code/executables/inc/JSONWebSocketServer.hpp
+++ code/executables/inc/JSONWebSocketServer.hpp
@@ -26,69 +26,80 @@ volatile sig_atomic_t stop;
 template<class ServiceT>
 class JSONWebSocketServer
 {
-public:
-    JSONWebSocketServer(const ServiceT& service, const bool verbose):
-            handler(service, verbose){}
-    virtual ~JSONWebSocketServer(){}
-
-    void start(const short unsigned int port, const bool debug)
-    {
-        std::cout << "Starting websocket server on " << ADDRESS << ":" << port << " (press Ctrl+C to terminate)" << std::endl;
-        std::shared_ptr<ssc::websocket::Server> w(new ssc::websocket::Server(handler, port, debug));
-        signal(SIGINT, inthand);
-        while(!stop){std::this_thread::sleep_for(std::chrono::milliseconds(500));}
-        std::cout << std::endl << "Gracefully stopping the websocket server..." << std::endl;
-    }
-
-private:
-    static void inthand(int)
-    {
-        stop = 1;
-    }
-
-    class JSONHandler : public ssc::websocket::MessageHandler
-    {
     public:
-        JSONHandler(const ServiceT& simserver, const bool verbose_):
-                sim_server(simserver),
-                verbose(verbose_){}
-        virtual ~JSONHandler(){}
-
-        void operator()(const ssc::websocket::Message& msg)
+        JSONWebSocketServer(const ServiceT& service, const bool verbose) : handler(service, verbose)
         {
-            const std::string input_json = msg.get_payload();
-            if (verbose)
+        }
+        virtual ~JSONWebSocketServer()
+        {
+        }
+
+        void start(const short unsigned int port, const bool debug)
+        {
+            std::cout << "Starting websocket server on " << ADDRESS << ":" << port << " (press Ctrl+C to terminate)" << std::endl;
+            std::shared_ptr<ssc::websocket::Server> w(new ssc::websocket::Server(handler, port, debug));
+            signal(SIGINT, inthand);
+            while(!stop)
             {
-                std::cout << current_date_time() << " Received: " << input_json << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
-            const std::function<void(const std::string&)> quiet_error_outputter = [&msg, this](const std::string& what) {msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
-            const std::function<void(const std::string&)> verbose_error_outputter = [&msg, this](const std::string& what) {std::cerr << current_date_time() << " Error: " << what << std::endl; msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
-            const auto error_outputter = verbose ? verbose_error_outputter : quiet_error_outputter;
-            const auto f = [&input_json, this, &msg]()
-                {
-                    SimServerInputs server_inputs(deserialize(input_json), sim_server.get_Tmax());
-                    const std::string output_json = serialize(sim_server.handle(server_inputs));
-                    if (verbose)
-                    {
-                        std::cout << current_date_time() << " Sending: " << output_json << std::endl;
-                    }
-                    msg.send_text(output_json);
-                };
-            report_xdyn_exceptions_to_user(f, error_outputter);
+            std::cout << std::endl << "Gracefully stopping the websocket server..." << std::endl;
         }
 
     private:
-        std::string replace_newlines_by_spaces(std::string str)
+        static void inthand(int)
         {
-            boost::replace_all(str, "\n", " ");
-            return str;
+            stop = 1;
         }
 
-        ServiceT sim_server;
-        const bool verbose;
-    };
+        class JSONHandler : public ssc::websocket::MessageHandler
+        {
+            public:
+                JSONHandler(const ServiceT& simserver, const bool verbose_) : sim_server(simserver), verbose(verbose_)
+                {
+                }
+                virtual ~JSONHandler()
+                {
+                }
 
-    JSONHandler handler;
+                void operator()(const ssc::websocket::Message& msg)
+                {
+                    const std::string input_json = msg.get_payload();
+                    if(verbose)
+                    {
+                        std::cout << current_date_time() << " Received: " << input_json << std::endl;
+                    }
+                    const std::function<void(const std::string&)> quiet_error_outputter = [&msg, this](const std::string& what)
+                    {   msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
+                    const std::function<void(const std::string&)> verbose_error_outputter = [&msg, this](
+                            const std::string& what)
+                            {   std::cerr << current_date_time() << " Error: " << what << std::endl; msg.send_text(replace_newlines_by_spaces(std::string("{\"error\": \"") + what + "\"}"));};
+                    const auto error_outputter = verbose ? verbose_error_outputter : quiet_error_outputter;
+                    const auto f = [&input_json, this, &msg]()
+                    {
+                        SimServerInputs server_inputs(deserialize(input_json), sim_server.get_Tmax());
+                        const std::string output_json = serialize(sim_server.handle(server_inputs));
+                        if (verbose)
+                        {
+                            std::cout << current_date_time() << " Sending: " << output_json << std::endl;
+                        }
+                        msg.send_text(output_json);
+                    };
+                    report_xdyn_exceptions_to_user(f, error_outputter);
+                }
+
+            private:
+                std::string replace_newlines_by_spaces(std::string str)
+                {
+                    boost::replace_all(str, "\n", " ");
+                    return str;
+                }
+
+                ServiceT sim_server;
+                const bool verbose;
+        };
+
+        JSONHandler handler;
 };
 
 #endif /* EXECUTABLES_INC_JSONWEBSOCKETSERVER_HPP_ */
