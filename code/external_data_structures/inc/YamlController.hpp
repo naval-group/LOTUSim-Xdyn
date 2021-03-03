9172fe186a9d6dcb53e331cc5bcf94cd52faeb0a
Lea Lincker <lea.lincker@sirehna.com>
2021-03-03 12:25:46 +0100

    :sparkle: Add a Yaml structure for controllers.
    'controllers' is a new key of the yaml,
    used to give the parameters of the controllers (that will be used by the controlled forces).

---
+++ code/external_data_structures/inc/YamlController.hpp
@@ -0,0 +1,30 @@
+/*
+ * YamlController.hpp
+ *
+ *  Created on: Fev, 10 2021
+ *      Author: lincker
+ */
+
+#ifndef YAMLCONTROLLER_HPP_
+#define YAMLCONTROLLER_HPP_
+
+#include <map>
+#include <string>
+#include <vector>
+
+/** \brief Stores the controller parameters used to generate a command for a controlled force
+ */
+struct YamlController
+{
+    YamlController();
+    std::string name;                      //!< Name of the controlled force
+    std::string output;                    //!< Name of the command outputed by the controller
+    std::string type;                      //!< Type of the controller (PID, gRPC)
+    double dt;                             //!< Time step of the discrete system
+    std::string input;                     //!< Name of the input needed by the controller as setpoint
+    std::map<std::string, double> states;  //!< States used by the controller as measurement (e.g. {{'u',1},{'v',-3}} for 'u - 3v')
+    std::string rest_of_the_yaml;          //!< All other fields that are spectific to the controller type
+};
+
+
+#endif /* YAMLCONTROLLER_HPP_ */
---
+++ code/external_data_structures/src/YamlController.cpp
@@ -0,0 +1,11 @@
+/*
+ * YamlController.cpp
+ *
+ *  Created on: Fev, 10 2021
+ *      Author: lincker
+ */
+
+#include "YamlController.hpp"
+
+YamlController::YamlController() : name(), output(), type(), dt(), input(), states(), rest_of_the_yaml()
+{}
