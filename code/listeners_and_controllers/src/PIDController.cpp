200a8c8f109d2980cfee8b5c7726b599ffe97a26
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-03-25 17:49:29 +0100

    :lipstick: Formatted PIDController.cpp with clang-format

--- code/listeners_and_controllers/src/PIDController.cpp
+++ code/listeners_and_controllers/src/PIDController.cpp
@@ -8,25 +8,23 @@
 #include "PIDController.hpp"
 #include "InvalidInputException.hpp"
 
-PIDController::PIDController(const double dt,
-                             const std::string& output_name,
-                             const std::string& input_name,
-                             const std::map<std::string, double>& states,
-                             const std::string& yaml) :
-                                 Controller(dt, output_name, input_name, states),
-                                 yaml(yaml), dt(dt), t_start(), i_previous_t(0),
-                                 initialized(false), previous_error(), integral_term(0)
+PIDController::PIDController (const double dt, const std::string &output_name,
+                              const std::string &input_name,
+                              const std::map<std::string, double> &states,
+                              const std::string &yaml)
+    : Controller (dt, output_name, input_name, states), yaml (yaml), dt (dt),
+      t_start (), i_previous_t (0), initialized (false), previous_error (),
+      integral_term (0)
 {
 }
 
-
-PIDController::Yaml::Yaml(const std::string& yaml) : Kp(), Ki(), Kd()
+PIDController::Yaml::Yaml (const std::string &yaml) : Kp (), Ki (), Kd ()
 {
-    std::stringstream stream(yaml);
+    std::stringstream stream (yaml);
     std::stringstream ss;
-    YAML::Parser parser(stream);
+    YAML::Parser parser (stream);
     YAML::Node node;
-    parser.GetNextDocument(node);
+    parser.GetNextDocument (node);
 
     try
     {
@@ -34,20 +32,25 @@ PIDController::Yaml::Yaml(const std::string& yaml) : Kp(), Ki(), Kd()
         node["gains"]["Ki"] >> Ki;
         node["gains"]["Kd"] >> Kd;
     }
-    catch (YAML::Exception& e)
+    catch (YAML::Exception &e)
     {
-        THROW(__PRETTY_FUNCTION__, InvalidInputException, "Unable to parse 'gains': it should contain the sub-nodes 'Kp', 'Ki' and 'Kd'.");
+        THROW (__PRETTY_FUNCTION__, InvalidInputException,
+               "Unable to parse 'gains': it should contain the sub-nodes "
+               "'Kp', 'Ki' and 'Kd'.");
     }
 }
 
-/** \brief Computes the command value from the input data, using a PID controller algorithm.
+/** \brief Computes the command value from the input data, using a PID
+ * controller algorithm.
  * https://en.wikipedia.org/wiki/PID_controller#Pseudocode
  */
-double PIDController::compute_command(const double setpoint, const double measured_value, const double t)
+double
+PIDController::compute_command (const double setpoint,
+                                const double measured_value, const double t)
 {
     const double error = setpoint - measured_value;
-    const double previous_t = get_previous_t();
-    const double next_meeting_point = get_current_t();
+    const double previous_t = get_previous_t ();
+    const double next_meeting_point = get_current_t ();
     double derivativeTerm = 0;
 
     // Proportional term
@@ -56,16 +59,19 @@ double PIDController::compute_command(const double setpoint, const double measur
     if (initialized && next_meeting_point <= t)
     {
         // Integral term
-        integral_term = integral_term + yaml.Ki * error * (next_meeting_point - previous_t);
+        integral_term = integral_term
+                        + yaml.Ki * error * (next_meeting_point - previous_t);
 
         // Derivative term
-        derivativeTerm = yaml.Kd * (error - previous_error) / (next_meeting_point - previous_t);
+        derivativeTerm = yaml.Kd * (error - previous_error)
+                         / (next_meeting_point - previous_t);
 
         ++i_previous_t;
     }
 
-    if (!initialized) {
-        t_start     = t;
+    if (!initialized)
+    {
+        t_start = t;
         initialized = true;
     }
 
@@ -75,17 +81,21 @@ double PIDController::compute_command(const double setpoint, const double measur
     return proportionalTerm + integral_term + derivativeTerm;
 }
 
-double PIDController::get_previous_t()
+double
+PIDController::get_previous_t ()
 {
-    if (!initialized) {
+    if (!initialized)
+    {
         return 0;
     }
     return t_start + dt * i_previous_t;
 }
 
-double PIDController::get_current_t()
+double
+PIDController::get_current_t ()
 {
-    if (!initialized) {
+    if (!initialized)
+    {
         return 0;
     }
     return t_start + dt * (i_previous_t + 1);
