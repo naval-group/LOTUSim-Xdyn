49b87e2897a4d0df6fdb41387b9c27f928e78d64
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-04-02 17:03:14 +0200

    :ok_hand: Extracted get_yaml

--- grpc_tests/controller/tests.py
+++ grpc_tests/controller/tests.py
@@ -6,20 +6,23 @@ import yaml
 import controller
 
 
-def test_can_call_pid_controller():
-    psi_co = math.pi / 3
-    dt = 2.3
-    yaml_string = yaml.dump(
+def get_yaml(dt):
+    """Generate the YAML expected by the PID controller."""
+    return yaml.dump(
         {
             "command": "beta",
             "type": "PID",
             "dt": dt,
-            "setpoint": psi_co,
-            "weights": {"psi": 1},
+            "setpoint": "psi_co",
+            "weights": {"x": 2, "phi": 2, "p": -1},
             "gains": {"Kp": -1, "Ki": 0, "Kd": -1}
         }
     )
-    pid_controller.PIDController(yaml_string, dt)
+
+
+def test_can_call_pid_controller():
+    dt = 2.3
+    pid_controller.PIDController(get_yaml(dt), dt)
 
 
 def test_should_get_the_right_number_of_inputs():
@@ -30,16 +33,7 @@ def test_should_get_the_right_number_of_inputs():
 
 def test_can_get_plant_output():
     dt = 2.3
-    yaml_string = yaml.dump(
-        {
-            "command": "beta",
-            "type": "PID",
-            "dt": dt,
-            "setpoint": "psi_co",
-            "weights": {"x": 2, "phi": 2, "p": -1},
-            "gains": {"Kp": -1, "Ki": 0, "Kd": -1}
-        })
-    pid = pid_controller.PIDController(yaml_string, dt)
+    pid = pid_controller.PIDController(get_yaml(dt), dt)
     states = controller.StatesEuler()
     states.x = 1.3
     states.phi = 2.7
