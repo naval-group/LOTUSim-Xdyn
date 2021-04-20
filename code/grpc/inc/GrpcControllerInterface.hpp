76c92bdab74947a724a06f9f1192433317913d77
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-04-20 17:50:20 +0200

    :sparkles: Implemented controller gRPC client interface

--- code/grpc/inc/GrpcControllerInterface.hpp
+++ code/grpc/inc/GrpcControllerInterface.hpp
@@ -1,11 +1,35 @@
 #ifndef GRPCCONTROLLERINTERFACEHPP
 #define GRPCCONTROLLERINTERFACEHPP
 
-#include <memory>
+#include "BodyStates.hpp"
+#include "EnvironmentAndFrames.hpp"
+
+enum class AngleRepresentation
+{
+    QUATERNION,    EULER_321
+};
+
+struct GrpcSetParametersResponse
+{
+    GrpcSetParametersResponse();
+    double                   date_of_first_callback; // Date at which the controller should be called for the first time. Will often be equal to just t0.
+    std::vector<std::string> setpoint_names;         // Name of the controller inputs (setpoints) which xdyn must supply.
+    AngleRepresentation      angle_representation;   // Does the controller need to be called with get_commands_quaternion or with get_commands_euler_321?
+    bool                     has_extra_observations; // If set to true, the controller's get_extra_observations will be called.
+};
+
+struct GrpcControllerResponse
+{
+    GrpcControllerResponse();
+    std::map<std::string,double> commands; // Commands computed by the controller
+    double next_call; // Date at which the solver should call the controller again
+};
+
 
 class GrpcControllerInterface
 {
     public:
+        virtual ~GrpcControllerInterface();
         struct Input
         {
             Input ();
@@ -16,11 +40,44 @@ class GrpcControllerInterface
             std::string yaml; //!< The whole YAML node as a string, passed to the
                               //!< controller's set_parameters gRPC method
         };
+        double get_date_of_first_callback() const;
 
-        static GrpcControllerInterface::Input parse (const std::string &yaml);
+        static std::shared_ptr<GrpcControllerInterface> build(const Input& input, const double t0);
+        static Input parse (const std::string &yaml);
+        virtual GrpcControllerResponse get_commands(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env) = 0;
+        std::map<std::string, double> get_extra_observations();
+        bool has_extra_observations() const;
+        std::vector<std::string> get_setpoint_names() const;
+
+    protected:
+        struct Impl;
+        std::shared_ptr<Impl> pimpl;
+        GrpcControllerInterface(const std::shared_ptr<Impl>& pimpl, const GrpcSetParametersResponse& set_parameters_response);
+
+    private:
+        GrpcControllerInterface() = delete;
+        GrpcSetParametersResponse set_parameters_response;
 
 };
 
+class Euler321GrpcController : public GrpcControllerInterface
+{
+    public:
+        Euler321GrpcController(const std::shared_ptr<Impl>& pimpl, const GrpcSetParametersResponse& set_parameters_response);
+        GrpcControllerResponse get_commands(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env);
+    private:
+        Euler321GrpcController() = delete;
+
+};
+
+class QuaternionGrpcController : public GrpcControllerInterface
+{
+    public:
+        QuaternionGrpcController(const std::shared_ptr<Impl>& pimpl, const GrpcSetParametersResponse& set_parameters_response);
+        GrpcControllerResponse get_commands(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env);
+    private:
+        QuaternionGrpcController() = delete;
+};
 
 
 #endif
--- code/grpc/src/GrpcControllerInterface.cpp
+++ code/grpc/src/GrpcControllerInterface.cpp
@@ -1,8 +1,213 @@
 #include "GrpcControllerInterface.hpp"
 #include "InvalidInputException.hpp"
 #include "yaml.h"
+#include <grpcpp/grpcpp.h>
+#include "controller.pb.h"
+#include "controller.grpc.pb.h"
 
 GrpcControllerInterface::Input::Input () : url (), name (), yaml () {}
+
+GrpcSetParametersResponse::GrpcSetParametersResponse ()
+    : date_of_first_callback (), setpoint_names (), angle_representation (),
+      has_extra_observations ()
+{
+}
+
+GrpcControllerResponse::GrpcControllerResponse () : commands (), next_call ()
+{
+}
+
+GrpcControllerResponse convert_controller_response(const ControllerResponse& response);
+GrpcControllerResponse convert_controller_response(const ControllerResponse& response)
+{
+    GrpcControllerResponse ret;
+    ret.next_call = response.next_call();
+    ret.commands = std::map<std::string, double>(response.commands().begin(),response.commands().end());
+    return ret;
+}
+
+
+SetParametersRequest build_set_parameters_request(const std::string& yaml, const double t0);
+SetParametersRequest build_set_parameters_request(const std::string& yaml, const double t0)
+{
+    SetParametersRequest ret;
+    ret.set_parameters(yaml);
+    ret.set_t0(t0);
+    return ret;
+}
+
+
+ControllerStatesQuaternion* build_quaternion_state(const double t, const BodyStates& states);
+ControllerStatesQuaternion* build_quaternion_state(const double t, const BodyStates& states)
+{
+    ControllerStatesQuaternion* ret = new ControllerStatesQuaternion();
+    ret->set_t(t);
+    ret->set_x(states.x());
+    ret->set_y(states.y());
+    ret->set_z(states.z());
+    ret->set_u(states.u());
+    ret->set_v(states.v());
+    ret->set_w(states.w());
+    ret->set_p(states.p());
+    ret->set_q(states.q());
+    ret->set_r(states.r());
+    ret->set_qr(states.qr());
+    ret->set_qi(states.qi());
+    ret->set_qj(states.qj());
+    ret->set_qk(states.qk());
+    return ret;
+}
+
+ControllerRequestQuaternion build_quaternion_request(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints);
+ControllerRequestQuaternion build_quaternion_request(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints)
+{
+    ControllerRequestQuaternion ret;
+    ret.set_allocated_states(build_quaternion_state(t, states));
+    ret.set_allocated_dstates_dt(build_quaternion_state(t, dstates_dt));
+    *ret.mutable_setpoints() = {setpoints.begin(), setpoints.end()};
+    return ret;
+}
+
+ControllerStatesEuler* build_euler321_state(const double t, const BodyStates& states, const YamlRotation& rot);
+ControllerStatesEuler* build_euler321_state(const double t, const BodyStates& states, const YamlRotation& rot)
+{
+    ControllerStatesEuler* ret = new ControllerStatesEuler();
+    ssc::kinematics::RotationMatrix R = Eigen::Quaternion<double>(states.qr(),states.qi(),states.qj(),states.qk()).matrix();
+    const ssc::kinematics::EulerAngles euler_angles = states.convert(R, rot);
+    ret->set_t(t);
+    ret->set_x(states.x());
+    ret->set_y(states.y());
+    ret->set_z(states.z());
+    ret->set_u(states.u());
+    ret->set_v(states.v());
+    ret->set_w(states.w());
+    ret->set_p(states.p());
+    ret->set_q(states.q());
+    ret->set_r(states.r());
+    ret->set_phi(euler_angles.phi);
+    ret->set_theta(euler_angles.theta);
+    ret->set_psi(euler_angles.psi);
+    return ret;
+}
+
+ControllerRequestEuler build_euler321_request(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env);
+ControllerRequestEuler build_euler321_request(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env)
+{
+    ControllerRequestEuler ret;
+    ret.set_allocated_states(build_euler321_state(t, states, env.rot));
+    ret.set_allocated_dstates_dt(build_euler321_state(t, dstates_dt, env.rot));
+    *ret.mutable_setpoints() = {setpoints.begin(), setpoints.end()};
+    return ret;
+}
+
+
+struct GrpcControllerInterface::Impl
+{
+    Impl(const GrpcControllerInterface::Input& input)
+    : stub(Controller::NewStub(grpc::CreateChannel(input.url, grpc::InsecureChannelCredentials())))
+    , yaml(input.yaml)
+    {}
+
+    GrpcSetParametersResponse set_parameters(const double t0)
+    {
+        SetParametersResponse response;
+        grpc::ClientContext context;
+        const grpc::Status status = stub->set_parameters(&context, build_set_parameters_request(yaml, t0), &response);
+        GrpcSetParametersResponse ret;
+        switch(response.angle_representation())
+        {
+            case SetParametersResponse_AngleRepresentation::SetParametersResponse_AngleRepresentation_EULER_321:
+                ret.angle_representation = AngleRepresentation::EULER_321;
+                break;
+            case SetParametersResponse_AngleRepresentation::SetParametersResponse_AngleRepresentation_QUATERNION:
+                ret.angle_representation = AngleRepresentation::QUATERNION;
+                break;
+            case SetParametersResponse_AngleRepresentation::SetParametersResponse_AngleRepresentation_SetParametersResponse_AngleRepresentation_INT_MIN_SENTINEL_DO_NOT_USE_:
+                break;
+            case SetParametersResponse_AngleRepresentation::SetParametersResponse_AngleRepresentation_SetParametersResponse_AngleRepresentation_INT_MAX_SENTINEL_DO_NOT_USE_:
+                break;
+        }
+        ret.date_of_first_callback = response.date_of_first_callback();
+        ret.has_extra_observations = response.has_extra_observations();
+        ret.setpoint_names.reserve(response.setpoint_names_size());
+        std::copy(response.setpoint_names().begin(), response.setpoint_names().end(), std::back_inserter(ret.setpoint_names));
+        return ret;
+    }
+
+    GrpcControllerResponse get_commands_quaternion(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& )
+    {
+        ControllerResponse response;
+        grpc::ClientContext context;
+        const ControllerRequestQuaternion request = build_quaternion_request(t, states, dstates_dt, setpoints);
+        const grpc::Status status = stub->get_commands_quaternion(&context, request, &response);
+        return convert_controller_response(response);
+    }
+
+    GrpcControllerResponse get_commands_euler321(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env)
+    {
+        ControllerResponse response;
+        grpc::ClientContext context;
+        const ControllerRequestEuler request = build_euler321_request(t, states, dstates_dt, setpoints, env);
+        const grpc::Status status = stub->get_commands_euler_321(&context, request, &response);
+        return convert_controller_response(response);
+    }
+
+    std::map<std::string, double> get_extra_observations()
+    {
+        ExtraObservationsResponse response;
+        grpc::ClientContext context;
+        const grpc::Status status = stub->get_extra_observations(&context, ExtraObservationsRequest(), &response);
+        return std::map<std::string, double>(response.extra_observations().begin(), response.extra_observations().end());
+    }
+
+
+    private:
+        std::unique_ptr<Controller::Stub> stub;
+        std::string yaml;
+};
+
+
+GrpcControllerInterface::GrpcControllerInterface(const std::shared_ptr<GrpcControllerInterface::Impl>& pimpl_, const GrpcSetParametersResponse& set_parameters_response_) :
+pimpl(pimpl_),
+set_parameters_response(set_parameters_response_)
+{}
+
+GrpcControllerInterface::~GrpcControllerInterface()
+{}
+
+double GrpcControllerInterface::get_date_of_first_callback() const
+{
+    return set_parameters_response.date_of_first_callback;
+}
+
+bool GrpcControllerInterface::has_extra_observations() const
+{
+    return set_parameters_response.has_extra_observations;
+}
+
+std::vector<std::string> GrpcControllerInterface::get_setpoint_names() const
+{
+    return set_parameters_response.setpoint_names;
+}
+
+std::shared_ptr<GrpcControllerInterface> GrpcControllerInterface::build(const GrpcControllerInterface::Input& input, const double t0)
+{
+    std::shared_ptr<GrpcControllerInterface::Impl> pimpl(new GrpcControllerInterface::Impl(input));
+    const GrpcSetParametersResponse res = pimpl->set_parameters(t0);
+    switch(res.angle_representation)
+    {
+        case AngleRepresentation::EULER_321:
+            return std::shared_ptr<GrpcControllerInterface>(new Euler321GrpcController(pimpl, res));
+            break;
+        case AngleRepresentation::QUATERNION:
+            return std::shared_ptr<GrpcControllerInterface>(new QuaternionGrpcController(pimpl, res));
+            break;
+        default:
+            return std::shared_ptr<GrpcControllerInterface>(new Euler321GrpcController(pimpl, res));
+            break;
+    }
+    return std::shared_ptr<GrpcControllerInterface>(new Euler321GrpcController(pimpl, res));
+}
 
 GrpcControllerInterface::Input
 GrpcControllerInterface::parse (const std::string &yaml)
@@ -32,3 +237,20 @@ GrpcControllerInterface::parse (const std::string &yaml)
     }
     return ret;
 }
+
+
+GrpcControllerResponse QuaternionGrpcController::get_commands(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env)
+{
+    return pimpl->get_commands_quaternion(t, states, dstates_dt, setpoints, env);
+}
+
+GrpcControllerResponse Euler321GrpcController::get_commands(const double t, const BodyStates& states, const BodyStates& dstates_dt, const std::vector<double>& setpoints, const EnvironmentAndFrames& env)
+{
+    return pimpl->get_commands_euler321(t, states, dstates_dt, setpoints, env);
+}
+
+Euler321GrpcController::Euler321GrpcController(const std::shared_ptr<Impl>& pimpl_, const GrpcSetParametersResponse& set_parameters_response) : GrpcControllerInterface(pimpl_, set_parameters_response)
+{}
+
+QuaternionGrpcController::QuaternionGrpcController(const std::shared_ptr<Impl>& pimpl_, const GrpcSetParametersResponse& set_parameters_response) : GrpcControllerInterface(pimpl_, set_parameters_response)
+{}
