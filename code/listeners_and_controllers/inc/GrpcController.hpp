6611aa092eb84653f203153a5a18ea3d73eda892
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-04-20 11:53:09 +0200

    :art: Ran formatter on GrpcController.hpp

--- code/listeners_and_controllers/inc/GrpcController.hpp
+++ code/listeners_and_controllers/inc/GrpcController.hpp
@@ -12,31 +12,32 @@
 
 class GrpcController : public Controller
 {
-    public:
-        struct Input
-        {
-            Input();
-            std::string url;
-            std::string name;
-            std::string yaml;
-        };
-        static Input parse(const std::string& yaml);
+  public:
+    struct Input
+    {
+        Input ();
+        std::string url;
+        std::string name;
+        std::string yaml;
+    };
+    static Input parse (const std::string &yaml);
 
-        GrpcController(const double tstart,
-                      const double dt,
-                      const std::string& yaml
-                      );
+    GrpcController (const double tstart, const double dt,
+                    const std::string &yaml);
 
-    private:
-        /**
-         * @brief Updates the controller output value in the datasource.
-         *
-         * This method will be called by the "ssc::solver::DiscreteSystem::callback" method.
-         * It is the only method we must override from the Controller class.
-         * @param time Current simulation time (in seconds).
-         * @param system The continuous system. Used to retrieve the continuous states.
-         */
-        void update_discrete_states(const double time, ssc::solver::ContinuousSystem* system);
+  private:
+    /**
+     * @brief Updates the controller output value in the datasource.
+     *
+     * This method will be called by the
+     * "ssc::solver::DiscreteSystem::callback" method. It is the only method we
+     * must override from the Controller class.
+     * @param time Current simulation time (in seconds).
+     * @param system The continuous system. Used to retrieve the continuous
+     * states.
+     */
+    void update_discrete_states (const double time,
+                                 ssc::solver::ContinuousSystem *system);
 };
 
 #endif /* GRPCCONTROLLER_HPP_ */
