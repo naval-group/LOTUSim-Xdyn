41e662fe2172d070296305b8082623e7b62a95f8
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-05-06 15:00:02 +0200

    :hammer: Moved SurfaceElevationInterface::evaluate_rao to DiffractionForceModel::Impl::evaluate_rao
    Computing the response from RAO and environmental models should not be the environmental model's responsibility: either it should be in the force model or in a dedicated class.

--- code/force_models/src/DiffractionForceModel.cpp
+++ code/force_models/src/DiffractionForceModel.cpp
@@ -11,6 +11,7 @@
 #include "DiffractionInterpolator.hpp"
 #include "HDBParser.hpp"
 #include "InvalidInputException.hpp"
+#include "InternalErrorException.hpp"
 #include "SurfaceElevationInterface.hpp"
 #include "yaml.h"
 #include "external_data_structures_parsers.hpp"
@@ -152,11 +153,12 @@ class DiffractionForceModel::Impl
                     }
                     try
                     {
-                        w((int)degree_of_freedom_idx) = env.w->evaluate_rao(position_in_ned_for_the_wave_model.x(),
+                        w((int)degree_of_freedom_idx) = evaluate_rao(position_in_ned_for_the_wave_model.x(),
                                                         position_in_ned_for_the_wave_model.y(),
                                                         t,
                                                         rao_modules[degree_of_freedom_idx],
-                                                        rao_phases[degree_of_freedom_idx]);
+                                                        rao_phases[degree_of_freedom_idx],
+                                                        env);
                     }
                     catch (const ssc::exception_handling::Exception& e)
                     {
@@ -166,6 +168,42 @@ class DiffractionForceModel::Impl
             }
             const auto ww = express_aquaplus_wrench_in_xdyn_coordinates(w);
             return ww;
+        }
+
+        double evaluate_rao(const double x, //!< x-position of the RAO's calculation point in the NED frame (in meters)
+                            const double y, //!< y-position of the RAO's calculation point in the NED frame (in meters)
+                            const double t, //!< Current time instant (in seconds)
+                            const std::vector<std::vector<double> >& rao_modules, //!< Module of the RAO (spectrum_index, flattened_omega_x_psi_index)
+                            const std::vector<std::vector<double> >& rao_phases, //!< Phase of the RAO (spectrum_index, flattened_omega_x_psi_index)
+                            const EnvironmentAndFrames& env) const
+        {
+            // The RAOs from the HDB file are interpolated by hdb_interpolators/DiffractionInterpolator
+            // called by class DiffractionForceModel::Impl's constructor which ensures that the first
+            // dimension of rao_phase & rao_module is the index of the directional spectrum and the
+            // second index is the position in the "flattened" (omega,psi) matrix. The RAO's are interpolated
+            // at the periods and incidences specified by each wave directional spectrum.
+            const auto directional_spectra = env.w->get_flat_directional_spectra(x, y, t);
+            double F = 0;
+            for (size_t spectrum_idx = 0 ; spectrum_idx < directional_spectra.size() ; ++spectrum_idx)
+            {
+                const std::vector<double> rao_module_for_each_frequency_and_incidence = rao_modules.at(spectrum_idx);
+                const std::vector<double> rao_phase_for_each_frequency_and_incidence = rao_phases.at(spectrum_idx);
+                const size_t nb_of_omegas_x_nb_of_directions = rao_module_for_each_frequency_and_incidence.size();
+                const auto spectrum = directional_spectra.at(spectrum_idx);
+                if (nb_of_omegas_x_nb_of_directions != spectrum.k.size())
+                {
+                    THROW(__PRETTY_FUNCTION__, InternalErrorException, "Number of angular frequencies times number of incidences in HDB RAO is " << nb_of_omegas_x_nb_of_directions << ", which does not match spectrum size (" << spectrum.k.size() << " (omega,psi) pairs)");
+                }
+                for (size_t i = 0 ; i < nb_of_omegas_x_nb_of_directions ; ++i) // For each (omega,beta) pair
+                {
+                    const double rao_amplitude = rao_module_for_each_frequency_and_incidence[i] * spectrum.a[i];
+                    const double omega_t = spectrum.omega[i] * t;
+                    const double k_xCosPsi_ySinPsi = spectrum.k[i] * (x * spectrum.cos_psi[i] + y * spectrum.sin_psi[i]);
+                    const double theta = spectrum.phase[i];
+                    F -= rao_amplitude * sin(-omega_t + k_xCosPsi_ySinPsi + theta + rao_phase_for_each_frequency_and_incidence[i]);
+                }
+            }
+            return F;
         }
 
         ssc::kinematics::Vector6d express_aquaplus_wrench_in_xdyn_coordinates(ssc::kinematics::Vector6d v) const
