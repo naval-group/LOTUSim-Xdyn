e2e7478b00c09fe8ee19b6445afcf259bca41de7
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-05-10 12:25:07 +0200

    :hammer: Generalized the use of at(...) for vector element access from flat spectra against operator[]
    Except for FlatDiscreteDirectionalWaveSpectrum::omega, since we're iterating over its size, we know we won't exceed its length.

--- code/force_models/src/DiffractionForceModel.cpp
+++ code/force_models/src/DiffractionForceModel.cpp
@@ -110,21 +110,21 @@ class DiffractionForceModel::Impl
                         const auto directional_spectra = env.w->get_flat_directional_spectra(x(0), x(1), t);
                         for (const auto spectrum:directional_spectra) // For each directional spectrum
                         {
-                            const size_t nb_of_period_incidence_pairs = spectrum.k.size();
+                            const size_t nb_of_period_incidence_pairs = spectrum.omega.size();
                             for (size_t omega_beta_idx = 0 ; omega_beta_idx < nb_of_period_incidence_pairs ; ++omega_beta_idx) // For each incidence and each period (omega[i[omega_beta_idx]], beta[j[omega_beta_idx]])
                             {
                                 // Wave vector, i.e. angular wave number k as a vector along the direction of propagation
-                                const Eigen::Vector2d k(spectrum.k[omega_beta_idx]*spectrum.cos_psi[omega_beta_idx], spectrum.k[omega_beta_idx]*spectrum.sin_psi[omega_beta_idx]);
+                                const Eigen::Vector2d k(spectrum.k.at(omega_beta_idx)*spectrum.cos_psi.at(omega_beta_idx), spectrum.k.at(omega_beta_idx)*spectrum.sin_psi.at(omega_beta_idx));
                                 // Period
-                                const double period = TWOPI/spectrum.omega.at(omega_beta_idx);
+                                const double period = TWOPI/spectrum.omega[omega_beta_idx];
                                 // Wave incidence
                                 const double beta = psi - spectrum.psi.at(omega_beta_idx);
                                 // Interpolate RAO module and phase for this axis, period and incidence
                                 const double rao_module = rao.interpolate_module(degree_of_freedom_idx, period, beta);
                                 const double rao_phase = -rao.interpolate_phase(degree_of_freedom_idx, period, beta);
                                 // Evaluate force
-                                const double rao_amplitude = rao_module * spectrum.a[omega_beta_idx];
-                                const double omega_t = spectrum.omega.at(omega_beta_idx) * t;
+                                const double rao_amplitude = rao_module * spectrum.a.at(omega_beta_idx);
+                                const double omega_t = spectrum.omega[omega_beta_idx] * t;
                                 const double k_x = k.dot(x);
                                 const double theta = spectrum.phase.at(omega_beta_idx);
                                 w((int)degree_of_freedom_idx) -= rao_amplitude * sin(-omega_t + k_x + theta + rao_phase);
