bb882421125a1260ddac101b4d5c1e1c73aca9bf
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-06-14 15:38:09 +0200

    :ok_hand: Fixed changelog generation
    Cf. https://gitlab.sirehna.com/sirehna/xdyn/-/merge_requests/101#note_61910
    For some reason, when using a merge commit as first commit in a range,
    git log shows all ther merged commits which meant that some changes
    appeared twice in the changelog. The solution found by Léa was to use
    git log's --merges flag (cf.
    https://git-scm.com/docs/git-log#Documentation/git-log.txt---merges)

--- changelog/changelog.sh
+++ changelog/changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "# $2 (`git log -1 --date=short --format=%ad $2`)"
 echo ""
-git log --pretty=oneline $1^..$2 | grep "Merge" | sed  "s/^\(.*\)branch '//g" | sed "s/'\(.*\)'//g" | sed "s/\([0-9]\+\)-\(.*\)/#\1 \2/g" | sed "s/-/ /g" | sed 's/\(#[0-9]\+\) \([a-z]\)/- \1 \u\2/g' | grep "^-"
+git log --pretty=oneline --merges $1..$2 | grep "Merge" | sed  "s/^\(.*\)branch '//g" | sed "s/'\(.*\)'//g" | sed "s/\([0-9]\+\)-\(.*\)/#\1 \2/g" | sed "s/-/ /g" | sed 's/\(#[0-9]\+\) \([a-z]\)/- \1 \u\2/g' | grep "^-"
 echo ""
 echo ""
