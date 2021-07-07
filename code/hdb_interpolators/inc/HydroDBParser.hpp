56644a7b22d9daf9f94a4b5e92baf6c9843eeb7f
Lea Lincker <lea.lincker@sirehna.com>
2021-07-07 08:20:07 +0000

    :hammer: Add added mass getter

--- code/hdb_interpolators/inc/HydroDBParser.hpp
+++ code/hdb_interpolators/inc/HydroDBParser.hpp
@@ -10,12 +10,14 @@
 
 #include <array>
 #include <vector>
+#include <Eigen/Dense>
 
 
 class HydroDBParser
 {
     public:
         virtual ~HydroDBParser() = default;
+        virtual Eigen::Matrix<double,6,6> get_added_mass() const = 0;
         virtual std::array<std::vector<std::vector<double> >,6 > get_diffraction_module_tables() const = 0;
         virtual std::array<std::vector<std::vector<double> >,6 > get_diffraction_phase_tables() const = 0;
         virtual std::vector<double> get_diffraction_module_periods() const = 0;
