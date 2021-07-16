7dbc5a59beb47ed692f670a1b53aae3116f278e6
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-07-16 11:58:35 +0200

    :hammer: Implemented class RaoInterpolator
    Which will replace DiffractionInterpolator.

---
+++ code/hdb_interpolators/inc/RaoInterpolator.hpp
@@ -0,0 +1,93 @@
+#ifndef _RAOINTERPOLATORHPP_
+#define _RAOINTERPOLATORHPP_
+
+#include <cstdlib> // For size_t
+#include <vector>
+#include <Eigen/Dense>
+#include <ssc/interpolation.hpp>
+typedef ssc::interpolation::TwoDimensionalInterpolationVariableStep Interpolator;
+class HydroDBParser;
+struct YamlRAO;
+
+struct RAO
+{
+    RAO() = delete;
+    static RAO for_diffraction(const HydroDBParser& parser);
+    const std::array<std::vector<std::vector<double> >,6 > module_tables;
+    const std::array<std::vector<std::vector<double> >,6 > phase_tables;
+    const std::vector<double> module_periods;
+    const std::vector<double> phase_periods;
+    const std::vector<double> module_incidence;
+    const std::vector<double> phase_incidence;
+
+    private:
+        RAO(const std::array<std::vector<std::vector<double> >,6 > module_tables_,
+            const std::array<std::vector<std::vector<double> >,6 > phase_tables_,
+            const std::vector<double> module_periods_,
+            const std::vector<double> phase_periods_,
+            const std::vector<double> module_incidence_,
+            const std::vector<double> phase_incidence_);
+};
+
+class RaoInterpolator
+{
+    public:
+        RaoInterpolator(const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
+                        const std::vector<double>& omega, //<! Angular frequencies in the wave spectrum (points at which to interpolate the HDB data)
+                        const std::vector<double>& psi, //<! Wave directions (points at which to interpolate the HDB data)
+                        const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
+        );
+        RaoInterpolator(const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
+                        const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
+        );
+        virtual ~RaoInterpolator();
+
+        /**  \brief Interpolates the RAO's module for the values of omega & psi in the wave spectrum
+             *  \returns RAO_module[i][j], where i is frequency index & j direction index
+             *  \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
+             */
+        std::vector<std::vector<double> > get_modules_cartesian(const size_t k //<! Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
+                                                                );
+
+        /**  \brief Interpolates the RAO's phase for the values of omega & psi in the wave spectrum
+             *  \returns RAO_phase[i][j], where i is frequency index & j direction index
+             *  \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
+             */
+        std::vector<std::vector<double> > get_phases_cartesian(const size_t k //<! Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
+                                                                );
+
+        /**  \brief Interpolates the RAO's module for a given axis, a wave period (*not* an encounter period)
+         *    and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
+         *    0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
+             *  \returns module of the rao for the axis, the frequency and the incidence
+             */
+        double interpolate_module(const size_t axis, const double Tp, double beta);
+
+        /**  \brief Interpolates the RAO's phase for a given axis, a wave period (*not* an encounter period)
+         *    and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
+         *    0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
+             *  \returns module of the rao for the axis, the frequency and the incidence
+             */
+        double interpolate_phase(const size_t axis, const double Tp, double beta);
+
+        std::vector<double> get_module_periods() const;
+        Eigen::Vector3d get_rao_calculation_point() const;
+        bool using_encounter_period() const;
+
+    private:
+        RaoInterpolator();
+        std::vector<std::vector<double> > get_array_cartesian(Interpolator& i) const;
+
+    public:
+        std::array<Interpolator,6> module; //!< 2D spline interpolation function (omega,psi) for RAO's module
+        std::array<Interpolator,6> phase; //!< 2D spline interpolation function (omega,psi) for RAO's phase
+        bool mirror;
+        std::vector<double> omegas;
+        std::vector<double> psis;
+        std::pair<double, double> period_bounds;
+        RAO rao;
+        Eigen::Vector3d rao_calculation_point;
+        bool use_encounter_period;
+};
+
+#endif
---
+++ code/hdb_interpolators/src/RaoInterpolator.cpp
@@ -0,0 +1,174 @@
+#include "RaoInterpolator.hpp"
+#include "InternalErrorException.hpp"
+#define _USE_MATH_DEFINE
+#include <cmath>
+#define PI M_PI
+
+#include "HydroDBParser.hpp"
+#include "YamlRAO.hpp"
+#include <algorithm>
+
+std::vector<double> reverse(std::vector<double> t);
+
+
+RaoInterpolator::~RaoInterpolator(){}
+
+RaoInterpolator::RaoInterpolator(const HydroDBParser& data, //!< Data read from the HDB or Precal_R file
+                                                 const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
+        ) : RaoInterpolator(data, {}, {}, diffraction_yaml)
+{}
+
+RAO RAO::for_diffraction(const HydroDBParser& parser)
+{
+    return RAO(parser.get_diffraction_module_tables(),
+               parser.get_diffraction_phase_tables(),
+               parser.get_diffraction_module_periods(),
+               parser.get_diffraction_phase_periods(),
+               parser.get_diffraction_module_psis(),
+               parser.get_diffraction_phase_psis());
+}
+
+RAO::RAO(const std::array<std::vector<std::vector<double> >,6 > module_tables_,
+    const std::array<std::vector<std::vector<double> >,6 > phase_tables_,
+    const std::vector<double> module_periods_,
+    const std::vector<double> phase_periods_,
+    const std::vector<double> module_incidence_,
+    const std::vector<double> phase_incidence_)
+    : module_tables(module_tables_)
+    , phase_tables(phase_tables_)
+    , module_periods(module_periods_)
+    , phase_periods(phase_periods_)
+    , module_incidence(module_incidence_)
+    , phase_incidence(phase_incidence_)
+        {}
+
+
+
+RaoInterpolator::RaoInterpolator(const HydroDBParser& parser, //!< Data read from the HDB or Precal_R file
+                                 const std::vector<double>& omega, //!< Angular frequencies in the wave spectrum (points at which to interpolate the HDB data)
+                                 const std::vector<double>& psi, //!< Wave directions (points at which to interpolate the HDB data)
+                                 const YamlRAO& yaml_rao //<! Contents of the force model's parsed YAML data
+        )
+        : module()
+        , phase()
+        , mirror(yaml_rao.mirror)
+        , omegas(omega)
+        , psis(psi)
+        , period_bounds()
+        , rao(RAO::for_diffraction(parser))
+        , rao_calculation_point(yaml_rao.calculation_point.x,yaml_rao.calculation_point.y,yaml_rao.calculation_point.z)
+        , use_encounter_period(false)
+{
+    const RAO rao = RAO::for_diffraction(parser);
+    std::reverse(omegas.begin(),omegas.end());
+    period_bounds.first = *std::min_element(rao.module_periods.begin(), rao.module_periods.end());
+    period_bounds.second = *std::max_element(rao.module_periods.begin(), rao.module_periods.end());
+    
+    // For each axis (X,Y,Z,phi,theta,psi)
+    for (size_t i = 0 ; i < 6 ; ++i)
+    {
+        // module.at(i) and phase.at(i) are vectors of vectors: each element in the vector of vectors corresponds to a frequency omega
+        // For each omega, we have a vector containing the RAO values for each incidence
+        module.at(i) = Interpolator(rao.module_periods, rao.module_incidence, rao.module_tables.at(i));
+        phase.at(i) = Interpolator(rao.phase_periods, rao.phase_incidence, rao.phase_tables.at(i));
+    }
+    if (yaml_rao.use_encounter_period.is_initialized())
+    {
+        use_encounter_period = yaml_rao.use_encounter_period.get();
+    }
+}
+
+bool RaoInterpolator::using_encounter_period() const
+{
+    return use_encounter_period;
+}
+
+std::vector<double> RaoInterpolator::get_module_periods() const
+{
+    return rao.module_periods;
+}
+
+std::vector<std::vector<double> > RaoInterpolator::get_array_cartesian(Interpolator& i) const
+{
+    std::vector<std::vector<double> > ret;
+    for (auto omega:omegas)
+    {
+        std::vector<double> v;
+        for (auto psi:psis)
+        {
+            if (mirror and (psi>PI)) v.push_back(i.f(2*PI/omega, 2*PI-psi));
+            else                     v.push_back(i.f(2*PI/omega, psi));
+        }
+        ret.push_back(v);
+    }
+    return ret;
+}
+
+std::vector<std::vector<double> > RaoInterpolator::get_modules_cartesian(const size_t k //!< Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
+                                                     )
+{
+    return get_array_cartesian(module.at(k));
+}
+
+std::vector<std::vector<double> > RaoInterpolator::get_phases_cartesian(const size_t k //!< Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
+                                         )
+{
+    return get_array_cartesian(phase.at(k));
+}
+
+double RaoInterpolator::interpolate_module(const size_t axis, const double Tp, double beta)
+{
+    double ret = 0;
+    beta = beta - 2*PI * std::floor( beta / (2*PI) );
+    try
+    {
+
+        if (mirror and (beta>PI))
+        {
+            ret = module.at(axis).f(Tp,2*PI-beta);
+            // Cf; bug 3227: when RAO are given from 0 to 180 deg in the HDB file and mirroring option is one,
+            // diffraction force Fy and torques Mx and Mz should have opposite signs
+            if ((axis == 1) or (axis == 3) or (axis == 5))
+            {
+                ret *= -1;
+            }
+        }
+        else
+        {
+            ret = module.at(axis).f(Tp,beta);
+        }
+    }
+    catch(std::exception& e)
+    {
+        std::stringstream ss;
+        const char* ax = "XYZKMN";
+        ss << "Error interpolating RAO module for diffraction force: axis " << ax[axis] << ", Tp = " << Tp << " s, beta = " << beta*180/PI << "°: "  << e.what();
+        THROW(__PRETTY_FUNCTION__, InternalErrorException, ss.str());
+    }
+    return ret;
+}
+
+double RaoInterpolator::interpolate_phase(const size_t axis, const double Tp, double beta)
+{
+    double ret = 0;
+    beta = beta - 2*PI * std::floor( beta / (2*PI) );
+    try
+    {
+        if (mirror and (beta>PI)) ret = phase.at(axis).f(Tp,2*PI-beta);
+        else                      ret = phase.at(axis).f(Tp,beta);
+    }
+    catch(std::exception& e)
+    {
+        std::stringstream ss;
+        const char* ax = "XYZKMN";
+        ss << "Error interpolating RAO phase for diffraction force: axis " << ax[axis] << ", Tp = " << Tp << " s, beta = " << beta*180/PI << "°: "  << e.what();
+        THROW(__PRETTY_FUNCTION__, InternalErrorException, ss.str());
+    }
+    return ret;
+}
+
+
+Eigen::Vector3d RaoInterpolator::get_rao_calculation_point() const
+{
+    return rao_calculation_point;
+}
