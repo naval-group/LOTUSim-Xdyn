a712a33c39500137297bb1ec03058a3994d7efbf
Lea Lincker <lea.lincker@sirehna.com>
2021-07-19 14:10:12 +0000

    :pencil: Rename DiffractionForceModelTest -> LinearFroudeKrylovForceModelTest in LinearFroudeKrylovForceModelTest.hpp documentation

--- code/force_models/inc/LinearFroudeKrylovForceModel.hpp
+++ code/force_models/inc/LinearFroudeKrylovForceModel.hpp
@@ -13,9 +13,9 @@
  *  \addtogroup model_wrappers
  *  \ingroup model_wrappers
  *  \section ex1 Example
- *  \snippet model_wrappers/unit_tests/src/DiffractionForceModelTest.cpp DiffractionForceModelTest example
+ *  \snippet model_wrappers/unit_tests/src/LinearFroudeKrylovForceModelTest.cpp LinearFroudeKrylovForceModelTest example
  *  \section ex2 Expected output
- *  \snippet model_wrappers/unit_tests/src/DiffractionForceModelTest.cpp DiffractionForceModelTest expected output
+ *  \snippet model_wrappers/unit_tests/src/LinearFroudeKrylovForceModelTest.cpp LinearFroudeKrylovForceModelTest expected output
  */
 class LinearFroudeKrylovForceModel : public AbstractRaoForceModel
 {
