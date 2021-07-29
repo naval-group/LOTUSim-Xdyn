b1b709ce416cc36c4f5097aa94cd91902d71c6bf
Moran Charlou <moran.charlou@ec-nantes.fr>
2021-07-29 14:17:49 +0000

    :lipstick: Converted tabulations to spaces.
    Also set the default constructor of MMGManeuveringForceModel to 'deleted' instead of making it private.

--- code/force_models/inc/MMGManeuveringForceModel.hpp
+++ code/force_models/inc/MMGManeuveringForceModel.hpp
@@ -9,54 +9,54 @@
 class MMGManeuveringForceModel : public ForceModel
 {
 public:
-	struct Input
-	{
-		Input();
-		YamlCoordinates application_point;
-		double Lpp;
-		double T;
-		double Xvv;
-		double Xrr;
-		double Xvr;
-		double Xvvvv;
-		double Yv;
-		double Yr;
-		double Yvvv;
-		double Yrvv;
-		double Yvrr;
-		double Yrrr;
-		double Nv;
-		double Nr;
-		double Nvvv;
-		double Nrvv;
-		double Nvrr;
-		double Nrrr;
-	};
-	MMGManeuveringForceModel(const Input& input, const std::string& body_name, const EnvironmentAndFrames& env);
-	Wrench get_force(const BodyStates& states, const double t, const EnvironmentAndFrames& env, const std::map<std::string,double>& commands) const;
-	static Input parse(const std::string& yaml);
-	static std::string model_name();
+    struct Input
+    {
+        Input();
+        YamlCoordinates application_point;
+        double Lpp;
+        double T;
+        double Xvv;
+        double Xrr;
+        double Xvr;
+        double Xvvvv;
+        double Yv;
+        double Yr;
+        double Yvvv;
+        double Yrvv;
+        double Yvrr;
+        double Yrrr;
+        double Nv;
+        double Nr;
+        double Nvvv;
+        double Nrvv;
+        double Nvrr;
+        double Nrrr;
+    };
+    MMGManeuveringForceModel(const Input& input, const std::string& body_name, const EnvironmentAndFrames& env);
+    MMGManeuveringForceModel() = delete;
+    Wrench get_force(const BodyStates& states, const double t, const EnvironmentAndFrames& env, const std::map<std::string,double>& commands) const;
+    static Input parse(const std::string& yaml);
+    static std::string model_name();
 
 private:
-	MMGManeuveringForceModel(); // Deactivating default constructor
-	double Lpp;
-	double T;
-	double Xvv;
-	double Xrr;
-	double Xvr;
-	double Xvvvv;
-	double Yv;
-	double Yr;
-	double Yvvv;
-	double Yrvv;
-	double Yvrr;
-	double Yrrr;
-	double Nv;
-	double Nr;
-	double Nvvv;
-	double Nrvv;
-	double Nvrr;
-	double Nrrr;
+    double Lpp;
+    double T;
+    double Xvv;
+    double Xrr;
+    double Xvr;
+    double Xvvvv;
+    double Yv;
+    double Yr;
+    double Yvvv;
+    double Yrvv;
+    double Yvrr;
+    double Yrrr;
+    double Nv;
+    double Nr;
+    double Nvvv;
+    double Nrvv;
+    double Nvrr;
+    double Nrrr;
 
 };
 
