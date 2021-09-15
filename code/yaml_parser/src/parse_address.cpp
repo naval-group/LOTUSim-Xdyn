ee47862231133cca38b83eb2ec50f6eb7b5a9006
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2021-09-15 11:22:26 +0200

    :hammer: Simplified parsing of port number

--- code/yaml_parser/src/parse_address.cpp
+++ code/yaml_parser/src/parse_address.cpp
@@ -42,19 +42,18 @@ YamlOutput build_YamlOutput_from_WS_URL(const std::string& url)
     {
         const std::string protocol(what[1].first, what[1].second);
         const std::string domain(what[2].first, what[2].second);
-        const std::string portAsString(what[3].first, what[3].second);
+        const int port = std::atoi(std::string(what[3].first, what[3].second).c_str());
+        if ( port <= 0 || port > 65535 )
+        {
+            THROW(__PRETTY_FUNCTION__, InvalidInputException, "Port: " << port << " is not valid. It should be between 0 and 65535");
+        }
+        out.port = (short unsigned int)port;
         if (protocol!="ws")
         {
             THROW(__PRETTY_FUNCTION__, InvalidInputException, "Only 'ws' is valid. wss is not implemented yet");
         }
         out.format = "ws";
         out.address = protocol+"://"+domain;
-        const int portAsInt = std::atoi(portAsString.c_str());
-        if ( portAsInt <= 0 || portAsInt > 65535 )
-        {
-            THROW(__PRETTY_FUNCTION__, InvalidInputException, "Port: " << portAsInt << " is not valid. It should be between 0 and 65535");
-        }
-        out.port = (short unsigned int)portAsInt;
     }
     else
     {
