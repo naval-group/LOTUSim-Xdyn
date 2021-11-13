b03654da5efc976147597deb4ec981f46f00c47b
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2021-11-13 22:10:13 +0100

    :sparkles: Added new bindings

--- code/wrapper_python/src/main.cpp
+++ code/wrapper_python/src/main.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/eigen.h>
 #include <pybind11/stl.h>
 #include <Eigen/Dense>
+#include <iostream>
 #include <map>
 #include <string>
 #include <vector>
@@ -12,7 +13,13 @@
 #include "core/inc/Wrench.hpp"
 #include "force_models/inc/HydrostaticForceModel.hpp"
 #include "force_models/inc/GravityForceModel.hpp"
+#include "ssc/ssc/kinematics/coriolis_and_centripetal.hpp"
+#include "ssc/ssc/kinematics/EulerAngles.hpp"
+#include "ssc/ssc/kinematics/KinematicTree.hpp"
 #include "ssc/ssc/kinematics/Point.hpp"
+#include "ssc/ssc/kinematics/RotationMatrix.hpp"
+#include "ssc/ssc/kinematics/Transform.hpp"
+#include "ssc/ssc/kinematics/Velocity.hpp"
 #include "ssc/ssc/kinematics/Wrench.hpp"
 
 #define STRINGIFY(x) #x
@@ -124,6 +131,64 @@ PYBIND11_MODULE(xdyn, m) {
         .def("get_frame",&ssc::kinematics::Wrench::get_frame)
         .def_readwrite("force", &ssc::kinematics::Wrench::force, py::return_value_policy::reference_internal)
         .def_readwrite("torque", &ssc::kinematics::Wrench::torque, py::return_value_policy::reference_internal);
+
+    py::class_<ssc::kinematics::EulerAngles>(m_ssc_kinematics, "EulerAngles")
+        .def(py::init<>())
+        .def(py::init<const double, const double, const double>())
+        .def_readwrite("phi", &ssc::kinematics::EulerAngles::phi)
+        .def_readwrite("theta", &ssc::kinematics::EulerAngles::theta)
+        .def_readwrite("psi", &ssc::kinematics::EulerAngles::psi)
+        .def("__repr__",
+           [](const ssc::kinematics::EulerAngles &a) {
+               std::stringstream ss;
+               ss << "{\"phi\":" << a.phi
+                  << ",\"theta\":" << a.theta
+                  << ",\"psi\":" << a.psi << "}";
+                return ss.str();
+            }
+        );
+
+    py::class_<ssc::kinematics::KinematicTree>(m_ssc_kinematics, "KinematicTree")
+        .def(py::init<>())
+        .def("add",&ssc::kinematics::KinematicTree::add)
+        .def("get_path",&ssc::kinematics::KinematicTree::get_path);
+
+
+    py::class_<ssc::kinematics::RotationMatrix>(m_ssc_kinematics, "RotationMatrix");
+
+    py::class_<ssc::kinematics::Transform>(m_ssc_kinematics, "Transform")
+        .def(py::init<>())
+        .def(py::init<const ssc::kinematics::Point& /*translation*/, const ssc::kinematics::RotationMatrix& /*rotation*/, const std::string& /*to_frame*/>())
+        .def(py::init<const ssc::kinematics::Point& /*translation*/, const std::string& /*to_frame*/>())
+        .def(py::init<const ssc::kinematics::RotationMatrix& /*rotation*/, const std::string& /*from_frame*/, const std::string& /*to_frame*/>())
+        .def("get_from_frame",&ssc::kinematics::Transform::get_from_frame)
+        .def("get_to_frame",&ssc::kinematics::Transform::get_to_frame)
+        .def("inverse",&ssc::kinematics::Transform::inverse)
+        .def("get_point",&ssc::kinematics::Transform::get_point)
+        .def("get_rot",&ssc::kinematics::Transform::get_rot);
+
+
+    py::class_<ssc::kinematics::Velocity>(m_ssc_kinematics, "Velocity")
+        .def(py::init<const ssc::kinematics::Point& /*p*/, const ssc::kinematics::AngularVelocityVector& /*w*/>())
+        .def(py::init<const ssc::kinematics::Point& /*p*/, const ssc::kinematics::TranslationVelocityVector& /*t*/, const ssc::kinematics::AngularVelocityVector& /*w*/>())
+        .def("get_rot",&ssc::kinematics::Velocity::get_frame)
+        .def("get_rot",&ssc::kinematics::Velocity::get_point)
+        .def("get_rot",&ssc::kinematics::Velocity::get_angular_velocity)
+        .def("get_rot",&ssc::kinematics::Velocity::get_translation_velocity)
+        .def("get_rot",&ssc::kinematics::Velocity::change_point)
+        ;
+
+    m_ssc_kinematics.def("coriolis_and_centripetal", &ssc::kinematics::coriolis_and_centripetal,
+         R"(
+        These are the forces due to the fact that the equation of Newton\'s
+        second law of motion is written in a non-Gallilean frame: hence its speed
+        must be taken into account. The inertia matrix provided is the
+        rigid body inertia. It should, in general, not be the added mass matrix or
+        the sum of the added mass matrix & the rigid-body inertia matrix because
+        the added mass forces are experimentally found not to be proportional to
+        the acceleration but they are proportional to the derivative of the projection
+        in the body frame of the linear velocity of the body with respect to the
+        (Gallilean) earth frame.)");
 
     m.def("add", &add, R"pbdoc(
         Add two numbers
