6cdf2547a33362e6720ee05483bce94a89eaba87
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2021-11-17 09:18:45 +0100

    :hammer: Reordered include files

--- code/wrapper_python/src/main.cpp
+++ code/wrapper_python/src/main.cpp
@@ -3,17 +3,17 @@
 #include <pybind11/iostream.h>
 #include <pybind11/stl.h>
 #include <Eigen/Dense>
-#include <boost/optional.hpp>
-// `boost::optional` as an example -- can be any `std::optional`-like container
-namespace pybind11 { namespace detail {
-    template <typename T>
-    struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
-}}
-
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
+#include <boost/optional.hpp>
+
+// Code to handle boost::optional usage in force_models
+namespace pybind11 { namespace detail {
+    template <typename T>
+    struct type_caster<boost::optional<T>> : optional_caster<boost::optional<T>> {};
+}}
 #include "demo.hpp"
 #include "core/inc/BodyStates.hpp"
 #include "core/inc/EnvironmentAndFrames.hpp"
