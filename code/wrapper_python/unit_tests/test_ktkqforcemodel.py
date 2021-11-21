d491b86e7850b55ee9f8d1d0384294a94214aa2d
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2021-11-21 11:17:49 +0100

    :sparkles: Added a first version for test_ktkqforcemodel.py

---
+++ code/wrapper_python/unit_tests/test_ktkqforcemodel.py
@@ -0,0 +1,100 @@
+"""
+Unit test for KtKqForceModel
+"""
+import unittest
+
+import numpy as np
+
+from xdyn import KtKqForceModel
+from xdyn.data.yaml import kt_kq
+
+
+class KtKqForceModelTest(unittest.TestCase):
+    """Test class for KtKqForceModel"""
+
+    def test_can_parse(self):
+        """Check that parse function produces a valid KtKqForceModelInput object"""
+        data = KtKqForceModel.parse(kt_kq())
+        self.assertEqual("port side propeller", data.name)
+        self.assertEqual(0, data.position_of_propeller_frame.angle.phi)
+        self.assertEqual(
+            -10 * np.pi / 180.0, data.position_of_propeller_frame.angle.theta
+        )
+        self.assertEqual(-1 * np.pi / 180.0, data.position_of_propeller_frame.angle.psi)
+        self.assertEqual(-4, data.position_of_propeller_frame.coordinates.x)
+        self.assertEqual(-2, data.position_of_propeller_frame.coordinates.y)
+        self.assertEqual(2, data.position_of_propeller_frame.coordinates.z)
+        self.assertEqual("mesh(body 1)", data.position_of_propeller_frame.frame)
+        self.assertEqual(1, data.relative_rotative_efficiency)
+        self.assertTrue(data.rotating_clockwise)
+        self.assertEqual(0.7, data.thrust_deduction_factor)
+        self.assertEqual(0.9, data.wake_coefficient)
+        self.assertEqual(2, data.diameter)
+        self.assertTrue(
+            np.allclose(
+                data.J,
+                np.array(
+                    [
+                        -1.0e00,
+                        -8.0e-1,
+                        -5.0e-1,
+                        -2.5e-1,
+                        -1.0e-3,
+                        1.0e-3,
+                        2.0e-1,
+                        4.0e-1,
+                        6.0e-1,
+                        7.0e-1,
+                        8.0e-1,
+                        1.0e00,
+                    ]
+                ),
+            )
+        )
+        self.assertTrue(
+            np.allclose(
+                data.Kt,
+                np.array(
+                    [
+                        -4.5e-1,
+                        -2.5e-1,
+                        -1.9e-1,
+                        -2.0e-1,
+                        -2.0e-1,
+                        3.25e-1,
+                        2.8e-1,
+                        2.33e-1,
+                        1.85e-1,
+                        1.62e-1,
+                        1.36e-1,
+                        8.5e-2,
+                    ]
+                ),
+            )
+        )
+        self.assertTrue(
+            np.allclose(
+                data.Kq,
+                np.array(
+                    [
+                        -4.8e-2,
+                        -3.3e-2,
+                        -2.2e-2,
+                        -2.5e-2,
+                        -2.8e-2,
+                        3.4e-2,
+                        3.26e-2,
+                        2.97e-2,
+                        2.55e-2,
+                        2.3e-2,
+                        2.04e-2,
+                        1.5e-2,
+                    ]
+                ),
+            )
+        )
+
+
+if __name__ == "__main__":
+
+    unittest.main()
