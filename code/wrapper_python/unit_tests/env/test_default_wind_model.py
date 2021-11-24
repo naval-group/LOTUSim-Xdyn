2d707b38d68a1ff08136f147cbba9d4f8453c9e0
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2021-11-24 18:43:56 +0100

    :sparkles: Added new unit test test_default_wind_model.py

---
+++ code/wrapper_python/unit_tests/env/test_default_wind_model.py
@@ -0,0 +1,28 @@
+"""
+Unit test for DefaultWindModel
+"""
+import unittest
+
+import numpy as np
+from xdyn.env import DefaultWindModel
+
+
+class DefaultWindModelTest(unittest.TestCase):
+    """Test class for DefaultWindModel"""
+
+    def setUp(self) -> None:
+        self.rng = np.random.default_rng(666)
+
+    def test_returns_no_wind(self):
+        wind_model = DefaultWindModel(0)
+        position = np.random.uniform(size=3)
+        time = np.random.uniform()
+        wind_vector = wind_model.get_wind(position, time)
+        self.assertEqual(0.0, wind_vector[0])
+        self.assertEqual(0.0, wind_vector[1])
+        self.assertEqual(0.0, wind_vector[2])
+
+
+if __name__ == "__main__":
+
+    unittest.main()
