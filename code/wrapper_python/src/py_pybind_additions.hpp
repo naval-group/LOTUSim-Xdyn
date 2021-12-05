fd90531f69bdf30239150ba65aa4cab07cdbbf5e
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2021-12-05 22:32:53 +0100

    :sparkles: Added support for lambda

--- code/wrapper_python/src/py_pybind_additions.hpp
+++ code/wrapper_python/src/py_pybind_additions.hpp
@@ -4,6 +4,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/cast.h>
 #include <pybind11/eigen.h>
+#include <pybind11/functional.h>
 #include <pybind11/iostream.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
