0226421c0fc530c35ba3a6ebfbffd27492434402
Moran Charlou <moran.charlou@ec-nantes.fr>
2022-02-21 15:44:05 +0000

    :white_check_mark: Added unit tests for FlettnerRotorForceModel.

---
+++ code/force_models/unit_tests/inc/FlettnerRotorForceModelTest.hpp
@@ -0,0 +1,13 @@
+#ifndef FORCE_MODELS_UNIT_TESTS_INC_FLETTNERROTORFORCEMODELTEST_HPP_
+#define FORCE_MODELS_UNIT_TESTS_INC_FLETTNERROTORFORCEMODELTEST_HPP_
+
+#include "gtest/gtest.h"
+
+class FlettnerRotorForceModelTest : public ::testing::Test
+{
+    public:
+        FlettnerRotorForceModelTest();
+        virtual ~FlettnerRotorForceModelTest() = default;
+};
+
+#endif /* FORCE_MODELS_UNIT_TESTS_INC_FLETTNERROTORFORCEMODELTEST_HPP_ */
