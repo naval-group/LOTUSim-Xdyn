66689849b88b4f3bb3c41a3684b8c372140ce868
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2022-03-29 17:02:59 +0200

    :white_check_mark: Refactored the test code
    So we can change the input CSV more easily.

--- code/listeners_and_controllers/unit_tests/src/CSVLineByLineReaderTest.cpp
+++ code/listeners_and_controllers/unit_tests/src/CSVLineByLineReaderTest.cpp
@@ -1,8 +1,13 @@
 #include "CSVLineByLineReaderTest.hpp"
 #include "CSVYaml.hpp"
+#include "TempFile.hpp"
 
-CSVLineByLineReaderTest::CSVLineByLineReaderTest()
-    : csv()
+CSVLineByLineReaderTest::CSVLineByLineReaderTest() : csv(), a(29081981)
+{
+}
+
+void happy_case(TempFile& csv);
+void happy_case(TempFile& csv)
 {
     csv << "t,some column,rpm_co,beta_co\n";
     csv << "0.2,23,65,78\n";
@@ -12,6 +17,53 @@ CSVLineByLineReaderTest::CSVLineByLineReaderTest()
     csv.close();
 }
 
+void single_line(TempFile& csv);
+void single_line(TempFile& csv)
+{
+    csv << "t,some column,rpm_co,beta_co\n";
+    csv << "0.2,23,65,78\n";
+    csv.close();
+}
+
+void empty(TempFile& csv);
+void empty(TempFile& csv)
+{
+    csv << "t,some column,rpm_co,beta_co\n";
+    csv.close();
+}
+
+void non_decreasing(TempFile& csv);
+void non_decreasing(TempFile& csv)
+{
+    csv << "t,some column,rpm_co,beta_co\n";
+    csv << "0.2,23,65,78\n";
+    csv << "2.001,28.5,6.65,4.778\n";
+    csv << "0.2001,27,665,778\n";
+    csv << "2.01,2.85,66.5,47.78\n";
+    csv.close();
+}
+
+void duplicate_column(TempFile& csv);
+void duplicate_column(TempFile& csv)
+{
+    csv << "t,rpm_co,rpm_co,beta_co\n";
+    csv << "0.2,23,65,78\n";
+    csv << "0.2001,27,665,778\n";
+    csv << "2.001,28.5,6.65,4.778\n";
+    csv << "2.01,2.85,66.5,47.78\n";
+    csv.close();
+}
+
+void non_existent_command_column(TempFile& csv);
+void non_existent_command_column(TempFile& csv)
+{
+    csv << "t,some column,rpm_co,beta\n";
+    csv << "0.2,23,65,78\n";
+    csv << "0.2001,27,665,778\n";
+    csv << "2.001,28.5,6.65,4.778\n";
+    csv << "2.01,2.85,66.5,47.78\n";
+    csv.close();
+}
 
 CSVLineByLineReaderTest::~CSVLineByLineReaderTest() {}
 
@@ -32,6 +84,7 @@ CSVLineByLineReader CSVLineByLineReaderTest::get_reader() const
 
 TEST_F(CSVLineByLineReaderTest, can_get_values_at_time_values_in_csv)
 {
+    happy_case(csv);
     CSVLineByLineReader reader = get_reader();
     std::unordered_map<std::string, double> values = reader.get_values(0.2);
     ASSERT_DOUBLE_EQ(65, values["port side propeller(rpm)"]);
@@ -46,6 +99,7 @@ TEST_F(CSVLineByLineReaderTest, can_get_values_at_time_values_in_csv)
 
 TEST_F(CSVLineByLineReaderTest, can_get_correct_value_for_time_between_timestamps)
 {
+    happy_case(csv);
     CSVLineByLineReader reader = get_reader();
     std::unordered_map<std::string, double> values = reader.get_values(0.20005);
     ASSERT_DOUBLE_EQ(65, values["port side propeller(rpm)"]);
