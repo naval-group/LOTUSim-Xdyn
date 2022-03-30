04a7ae07e4e9bc00dfac6227e36be6b08870814a
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2022-03-30 17:52:54 +0200

    :wrench: Fixed ninja_debug.sh
    Fixes the following error:
    
    ninja: error: '/opt/grpc/lib/libgrpc++_unsecure.a', needed by 'run_all_tests', missing and no known rule to make it

--- ninja_debug.sh
+++ ninja_debug.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 make headers
 docker run \
+    --name xdyn-ninja-debian \
     -t \
     --rm \
     -u $( id -u ):$( id -g ) \
     -v $(pwd)/build_deb11_dbg:/build \
     -w /build \
     -v $(pwd):/opt/share \
-    sirehna/base-image-debian11-gcc10:2021-08-17 \
+    sirehna/base-image-debian11-gcc10:2021-12-12 \
     /bin/sh -c "cd /build && ninja `echo $*`"
