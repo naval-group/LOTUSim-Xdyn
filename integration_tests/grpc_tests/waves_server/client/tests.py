ecf1b3555ac38e434d2c91d8004e96285ae7559d
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-05-05 21:42:09 +0200

    :hammer: Refactored gRPC wave server test

--- integration_tests/grpc_tests/waves_server/client/tests.py
+++ integration_tests/grpc_tests/waves_server/client/tests.py
@@ -30,34 +30,125 @@ environment models:
       model: no waves
 """
 
+DATA_WAVE_MONOCHROMATIC = \
+"""
+rotations convention: [psi, theta', phi'']
+environmental constants:
+    g: {value: 9.81, unit: m/s^2}
+    rho: {value: 1000, unit: kg/m^3}
+    nu: {value: 1.18e-6, unit: m^2/s}
+environment models:
+    - constant sea elevation in NED frame:
+        unit: m
+        value: 0
+      model: no waves
+"""
+
+DATA_WAVE_TWO_SPECTRA = \
+"""
+rotations convention: [psi, theta', phi'']
+
+environmental constants:
+    g: {value: 9.81, unit: m/s^2}
+    rho: {value: 1026, unit: kg/m^3}
+    nu: {value: 1.18e-6, unit: m^2/s}
+environment models:
+  - model: waves
+    discretization:
+       ndir: 128
+       nfreq: 128
+       omega min: {value: 0.1, unit: rad/s}
+       omega max: {value: 6, unit: rad/s}
+       energy fraction: 0.999
+       equal energy bins: false
+    spectra:
+      - model: airy
+        depth: {value: 100, unit: m}
+        seed of the random data generator: 0
+        stretching:
+          delta: 1
+          h: {unit: m, value: 0}
+        directional spreading:
+           type: dirac
+           waves propagating to: {value: 90, unit: deg}
+        spectral density:
+           type: jonswap
+           Hs: {value: 5, unit: m}
+           Tp: {value: 15, unit: s}
+           gamma: 1.2
+      - model: airy
+        depth: {value: 100, unit: m}
+        seed of the random data generator: 10
+        stretching:
+          delta: 1
+          h: {unit: m, value: 0}
+        directional spreading:
+           type: cos2s
+           s: 2
+           waves propagating to: {value: 90, unit: deg}
+        spectral density:
+           type: dirac
+           omega0: {value: 0.05, unit: rad/s}
+           Hs: {value: 15, unit: m}
+"""
+
+
 def test_01_no_wave():
     """Launch the server & run some gRPC calls."""
-    with grpc.insecure_channel('server:50051') as channel:
+    with grpc.insecure_channel('waves-server:50051') as channel:
         parameters = DATA_NO_WAVE
         LOGGER.info('Creating Waves instance')
         waves = Waves(channel, parameters)
         elevations_request = {'t': 5, 'points': [(1, 2), (3, 5)]}
-        LOGGER.info('Elevations for %s', json.dumps(elevations_request))
         elevations = waves.elevations(elevations_request)
-        expected_elevations  = [{'t':5,'x':1,'y':2,'z':0.0}, {'t':5,'x':2,'y':5,'z':0.0}]
-        for expected_elevation in expected_elevations:
-            assert np.allclose(expected_elevation['z'], 0.0)
-        LOGGER.info('Got following elevations from server: %s', json.dumps(elevations))
+        for elevation in elevations:
+            assert np.allclose(elevation['z'], 0.0)
         orbital_velocities_request = {'t': 5, 'points': [(1, 2, 10), (3, 5, 15)]}
         orbital_velocities = waves.orbital_velocities(orbital_velocities_request)
-        LOGGER.info('Got following orbital velocities from server: %s', json.dumps(orbital_velocities))
+        for orbital_velocity in orbital_velocities:
+            assert np.allclose(orbital_velocity['vx'], 0.0)
+            assert np.allclose(orbital_velocity['vy'], 0.0)
+            assert np.allclose(orbital_velocity['vz'], 0.0)
         dynamic_pressures_request = {'t': 5, 'points': [(1, 2, 10), (3, 5, 15)]}
         dynamic_pressures = waves.dynamic_pressures(dynamic_pressures_request)
-        LOGGER.info('Got following dynamic pressures from server: %s', json.dumps(dynamic_pressures))
+        for dynamic_pressure in dynamic_pressures:
+            assert np.allclose(dynamic_pressure['pdyn'], 0.0)
         spectrum = waves.spectrum()
-        LOGGER.info('Got following spectrum from server: %s', json.dumps(spectrum))
+        assert spectrum == []
         angular_frequencies_for_rao = waves.angular_frequencies_for_rao()
-        LOGGER.info('Got following angular_frequencies_for_rao from server: %s', json.dumps(angular_frequencies_for_rao))
+        assert angular_frequencies_for_rao == []
         directions_for_rao = waves.directions_for_rao()
-        LOGGER.info('Got following directions_for_rao from server: %s', json.dumps(directions_for_rao))
+        assert directions_for_rao == []
+
+def test_02_two_spectra():
+    with grpc.insecure_channel('waves-server:50051') as channel:
+        parameters = DATA_WAVE_TWO_SPECTRA
+        LOGGER.info('Creating Waves instance')
+        waves = Waves(channel, parameters)
+        elevations_request = {'t': 5, 'points': [(1, 2), (3, 5)]}
+        elevations = waves.elevations(elevations_request)
+        for elevation in elevations:
+            assert not np.allclose(elevation['z'], 0.0)
+        orbital_velocities_request = {'t': 5, 'points': [(1, 2, 10), (3, 5, 15)]}
+        orbital_velocities = waves.orbital_velocities(orbital_velocities_request)
+        for orbital_velocity in orbital_velocities:
+            assert not np.allclose(orbital_velocity['vx'], 0.0)
+            assert not np.allclose(orbital_velocity['vy'], 0.0)
+            assert not np.allclose(orbital_velocity['vz'], 0.0)
+        dynamic_pressures_request = {'t': 5, 'points': [(1, 2, 10), (3, 5, 15)]}
+        dynamic_pressures = waves.dynamic_pressures(dynamic_pressures_request)
+        for dynamic_pressure in dynamic_pressures:
+            assert not np.allclose(dynamic_pressure['pdyn'], 0.0)
+        spectrum = waves.spectrum()
+        assert spectrum != []
+        angular_frequencies_for_rao = waves.angular_frequencies_for_rao()
+        assert angular_frequencies_for_rao != []
+        directions_for_rao = waves.directions_for_rao()
+        assert directions_for_rao != []
 
 def main():
     test_01_no_wave()
+    test_02_two_spectra()
 
 if __name__ =="__main__":
     main()
