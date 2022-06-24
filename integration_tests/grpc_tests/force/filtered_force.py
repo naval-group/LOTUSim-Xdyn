15b43fd71778fabb23a47db62d4daa753a5bdd7d
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-06-24 12:16:07 +0200

    :books: Updated get_parameters method

--- integration_tests/grpc_tests/force/filtered_force.py
+++ integration_tests/grpc_tests/force/filtered_force.py
@@ -3,6 +3,8 @@
 import numpy as np
 import force
 import logging
+from typing import Any, Dict
+
 SERVICE_NAME = "grpc-force"
 
 logging.basicConfig(
@@ -18,12 +20,36 @@ LOGGER.setLevel(logging.INFO)
 class ForceModel(force.Model):
     """Using filtered states."""
 
-    def __init__(self, _, body_name, __):
+    def __init__(self, _, body_name: str, __):
         """Initialize parameters from gRPC's set_parameters."""
         self.body_name = body_name
 
-    def get_parameters(self):
-        """Just using defaults."""
+    def get_parameters(self) -> Dict[str, Any]:
+        """
+        Return a dictionary with all parameters needed by xdyn, with keys:
+
+        - `max_history_length` (float) How far back (in seconds) should the history values in
+           ForceRequest go?
+        - `needs_wave_outputs` (bool) Should the force model be queried at each time step using
+           the `required_wave_information` rpc method to know what wave information it requires?
+        - `frame` (str) Reference frame from which we define the reference in which the forces
+           and torques are expressed.
+        - `x` (float) Position along the x-axis of 'frame' of the point of application of the force.
+        - `y` (float) Position along the y-axis of 'frame' of the point of application of the force.
+        - `z` (float) Position along the z-axis of 'frame' of the point of application of the force.
+        - `phi` (float) First Euler angle in radian.
+        - `theta` (float) Second Euler angle in radian.
+        - `psi` (float) Third Euler angle in radian.
+        - `required_commands` (List[str]) List of commands needed by this model, without the model
+           name (e.g. ['beta1', 'beta2'])
+
+        `phi`, `theta`, `psi` are the three Euler angles defining the rotation from 'frame' to
+        the reference frame in which the forces and torques are expressed. Depends on the angle
+        convention chosen in the 'rotations convention' section of xdyn's input file.
+        See xdyn's documentation for details.
+
+        Just using defaults.
+        """
         # Initialize random seed once per simulation to get predictable results
         np.random.seed(0)
         return {'max_history_length': 0, 'needs_wave_outputs': False,
