8c78ef81101bc6f4f6626b5d7f9d018f2a0c7770
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-01 14:15:25 +0200

    :pencil: Typo update

--- code/environment_models/inc/discretize.hpp
+++ code/environment_models/inc/discretize.hpp
@@ -15,8 +15,8 @@ class Stretching;
 class WaveSpectralDensity;
 class WaveDirectionalSpreading;
 
-FlatDiscreteDirectionalWaveSpectrum flatten(const DiscreteDirectionalWaveSpectrum& spectrum
-                                            );
+FlatDiscreteDirectionalWaveSpectrum flatten(
+    const DiscreteDirectionalWaveSpectrum& spectrum);
 
 /**  \brief Only select the most important spectrum components & create single vector
   *  \details No need to loop on all frequencies & all directions: we only select
@@ -26,9 +26,9 @@ FlatDiscreteDirectionalWaveSpectrum flatten(const DiscreteDirectionalWaveSpectru
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest flatten_example
   */
 FlatDiscreteDirectionalWaveSpectrum filter(
-        const FlatDiscreteDirectionalWaveSpectrum& spectrum, //!< Spectrum to filter
-        const double energy_ratio = 1.0//!< Between 0 & 1: where should we cut off the spectra?
-        );
+    const FlatDiscreteDirectionalWaveSpectrum& spectrum, //!< Spectrum to filter
+    const double energy_ratio = 1.0//!< Between 0 & 1: where should we cut off the spectra? 0 -> Removes all rays, 1 -> Keeps all rays
+    );
 
 /**  \author cec
   *  \date Aug 1, 2014, 5:04:24 PM
@@ -39,15 +39,16 @@ FlatDiscreteDirectionalWaveSpectrum filter(
   *  \section ex1 Expected output
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest expected output
   */
-DiscreteDirectionalWaveSpectrum discretize(const WaveSpectralDensity& S,      //!< Frequency spectrum
-                                           const WaveDirectionalSpreading& D, //!< Spatial spectrum
-                                           const double omega_min,            //!< Upper bound of the angular frequency range (in rad/s)
-                                           const double omega_max,            //!< Upper bound of the angular frequency range (in rad/s)
-                                           const size_t nfreq,                //!< Number of frequencies in discrete spectrum
-                                           const size_t ndir,                 //!< Number of directions in discrete spectrum
-                                           const Stretching& stretching,      //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
-                                           const bool equal_energy_bins       //!< Choose omegas so the integral of S between two successive omegas is constant
-                                           );
+DiscreteDirectionalWaveSpectrum discretize(
+    const WaveSpectralDensity& S,      //!< Frequency spectrum
+    const WaveDirectionalSpreading& D, //!< Spatial spectrum
+    const double omega_min,            //!< Upper bound of the angular frequency range (in rad/s)
+    const double omega_max,            //!< Upper bound of the angular frequency range (in rad/s)
+    const size_t nfreq,                //!< Number of frequencies in discrete spectrum
+    const size_t ndir,                 //!< Number of directions in discrete spectrum
+    const Stretching& stretching,      //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
+    const bool equal_energy_bins       //!< Choose omegas so the integral of S between two successive omegas is constant
+    );
 
 /**  \author cec
   *  \date Aug 1, 2014, 5:04:24 PM
@@ -55,16 +56,17 @@ DiscreteDirectionalWaveSpectrum discretize(const WaveSpectralDensity& S,      //
   *  \details Finite depth hypothesis (for the wave number)
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest discretize_example
   */
-DiscreteDirectionalWaveSpectrum discretize(const WaveSpectralDensity& S,      //!< Frequency spectrum
-                                           const WaveDirectionalSpreading& D, //!< Spatial spectrum
-                                           const double omega_min,            //!< Upper bound of the angular frequency range (in rad/s)
-                                           const double omega_max,            //!< Upper bound of the angular frequency range (in rad/s)
-                                           const size_t nfreq,                //!< Number of frequencies in discrete spectrum
-                                           const size_t ndir,                 //!< Number of directions in discrete spectrum
-                                           const double h,                    //!< Water depth (in meters)
-                                           const Stretching& stretching,      //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
-                                           const bool equal_energy_bins       //!< Choose omegas so the integral of S between two successive omegas is constant
-                                           );
+DiscreteDirectionalWaveSpectrum discretize(
+    const WaveSpectralDensity& S,      //!< Frequency spectrum
+    const WaveDirectionalSpreading& D, //!< Spatial spectrum
+    const double omega_min,            //!< Upper bound of the angular frequency range (in rad/s)
+    const double omega_max,            //!< Upper bound of the angular frequency range (in rad/s)
+    const size_t nfreq,                //!< Number of frequencies in discrete spectrum
+    const size_t ndir,                 //!< Number of directions in discrete spectrum
+    const double h,                    //!< Water depth (in meters)
+    const Stretching& stretching,      //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
+    const bool equal_energy_bins       //!< Choose omegas so the integral of S between two successive omegas is constant
+    );
 
 /**  \author cady
  *  \date Nov 23, 2020
@@ -82,9 +84,9 @@ DiscreteDirectionalWaveSpectrum discretize(const WaveSpectralDensity& S,      //
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest equal_area_abscissae_expected_output
 */
 std::vector<double> equal_area_abscissae(
-        const std::vector<double>& xs, //!< Input abscissae at which the function is defined
-        const std::vector<double>& ys //!< Value of the function for each xs
-         );
+    const std::vector<double>& xs, //!< Input abscissae at which the function is defined
+    const std::vector<double>& ys //!< Value of the function for each xs
+    );
 
 /**
  * \author cady
@@ -106,38 +108,45 @@ std::vector<double> area_curve(const std::vector<double>& xs, const std::vector<
  * \param as Area curve.
  * \return Abscissa at which the integral has the given value.
  */
-double find_integration_bound_yielding_target_area(const double target_area, const std::vector<double>& xs, const std::vector<double>& ys, const std::vector<double>& as);
+double find_integration_bound_yielding_target_area(
+    const double target_area,
+    const std::vector<double>& xs,
+    const std::vector<double>& ys,
+    const std::vector<double>& as);
 
 /**  \brief Utility function used by the discretize function. Infinite depth approximation. This is where the stretching is taken into account.
   *  \returns Factor \f$f(k,z)\f$ such that \f$p_{\mbox{dyn}}=\rho g \eta_a f(k,z)\f$ (no unit), infinite depth approximation
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest dynamic_pressure_factor example
   */
-double dynamic_pressure_factor(const double k,              //!< Wave number (in 1/m)
-                               const double z,              //!< z-position in the NED frame (in meters)
-                               const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
-                               const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
-                              );
+double dynamic_pressure_factor(
+    const double k,              //!< Wave number (in 1/m)
+    const double z,              //!< z-position in the NED frame (in meters)
+    const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
+    const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
+);
 
 /**  \brief Utility function used by the discretize function. Finite depth. This is where the stretching is taken into account.
   *  \returns Factor \f$f(k,z,h)\f$ such that \f$p_{\mbox{dyn}}=\rho g \eta_a f(k,z,h)\f$ (no unit), finite depth.
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest dynamic_pressure_factor example
   */
-double dynamic_pressure_factor(const double k,              //!< Wave number (in 1/m)
-                               const double z,              //!< z-position in the NED frame (in meters)
-                               const double h,              //!< Average water depth (in meters)
-                               const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
-                               const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
-                              );
+double dynamic_pressure_factor(
+    const double k,              //!< Wave number (in 1/m)
+    const double z,              //!< z-position in the NED frame (in meters)
+    const double h,              //!< Average water depth (in meters)
+    const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
+    const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
+);
 
 /**  \brief Utility function used by the discretize function. Finite depth.
   *  \returns Factor \f$f(k,z,h)\f$ such that \f$p_{\mbox{dyn}}=\rho g \eta_a f(k,z,h)\f$ (no unit), finite depth.
   *  \snippet environment_models/unit_tests/src/discretizeTest.cpp discretizeTest dynamic_pressure_factor example
   */
-double dynamic_pressure_factor_sh(const double k,              //!< Wave number (in 1/m)
-                                  const double z,              //!< z-position in the NED frame (in meters)
-                                  const double h,              //!< Average water depth (in meters)
-                                  const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
-                                  const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
-                                 );
+double dynamic_pressure_factor_sh(
+    const double k,              //!< Wave number (in 1/m)
+    const double z,              //!< z-position in the NED frame (in meters)
+    const double h,              //!< Average water depth (in meters)
+    const double eta,            //!< Wave elevation at (x,y) in the NED frame (in meters)
+    const Stretching& stretching //!< Dilate z-axis to properly compute orbital velocities (delta-stretching)
+);
 
 #endif /* DISCRETIZE_HPP_ */
