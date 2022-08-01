d14e777b2a0320ddc8e59bd7cf52b2b69c40d43d
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-01 14:52:38 +0200

    :pencil: Typo update

--- code/hdb_interpolators/inc/RaoInterpolator.hpp
+++ code/hdb_interpolators/inc/RaoInterpolator.hpp
@@ -14,42 +14,49 @@ class HydroDBParser;
 class RaoInterpolator
 {
     public:
-        RaoInterpolator(const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
-                        const std::vector<double>& omega, //<! Angular frequencies in the wave spectrum (points at which to interpolate the HDB data)
-                        const std::vector<double>& psi, //<! Wave directions (points at which to interpolate the HDB data)
-                        const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
-        );
-        RaoInterpolator(const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
-                        const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
+        RaoInterpolator(
+            const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
+            const std::vector<double>& omega, //<! Angular frequencies in the wave spectrum (points at which to interpolate the HDB data)
+            const std::vector<double>& psi, //<! Wave directions (points at which to interpolate the HDB data)
+            const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
+            );
+        RaoInterpolator(
+            const HydroDBParser& data, //<! Data read from the HDB or Precal_R file
+            const YamlRAO& diffraction_yaml //<! Contents of the force model's parsed YAML data
         );
         virtual ~RaoInterpolator();
 
-        /**  \brief Interpolates the RAO's module for the values of omega & psi in the wave spectrum
-             *  \returns RAO_module[i][j], where i is frequency index & j direction index
-             *  \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
-             */
+        /**
+         * \brief Interpolates the RAO's module for the values of omega & psi in the wave spectrum
+         * \returns RAO_module[i][j], where i is frequency index & j direction index
+         * \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
+         */
         std::vector<std::vector<double> > get_modules_cartesian(const size_t k //<! Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
                                                                 );
 
-        /**  \brief Interpolates the RAO's phase for the values of omega & psi in the wave spectrum
-             *  \returns RAO_phase[i][j], where i is frequency index & j direction index
-             *  \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
-             */
-        std::vector<std::vector<double> > get_phases_cartesian(const size_t k //<! Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
-                                                                );
+        /**
+         * \brief Interpolates the RAO's phase for the values of omega & psi in the wave spectrum
+         * \returns RAO_phase[i][j], where i is frequency index & j direction index
+         * \snippet hdb_interpolators/unit_tests/src/RadiationInterpolatorTest.cpp RadiationInterpolatorTest method_example
+         */
+        std::vector<std::vector<double> > get_phases_cartesian(
+            const size_t k //<! Axis index (0 for Fx, 1 for Fy, 2 for Fz, 3 for Mx, 4 for My and 5 for Mz)
+            );
 
-        /**  \brief Interpolates the RAO's module for a given axis, a wave period (*not* an encounter period)
-         *    and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
-         *    0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
-             *  \returns module of the rao for the axis, the frequency and the incidence
-             */
+        /**
+         * \brief Interpolates the RAO's module for a given axis, a wave period (*not* an encounter period)
+         * and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
+         * 0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
+         * \returns module of the rao for the axis, the frequency and the incidence
+         */
         double interpolate_module(const size_t axis, const double Tp, double beta);
 
-        /**  \brief Interpolates the RAO's phase for a given axis, a wave period (*not* an encounter period)
-         *    and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
-         *    0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
-             *  \returns module of the rao for the axis, the frequency and the incidence
-             */
+        /**
+         * \brief Interpolates the RAO's phase for a given axis, a wave period (*not* an encounter period)
+         * and an incidence (between 0 and π for waves propagating towards port side, π and 2π for waves propagating towards starboard
+         * 0 for waves propagating from aft to fore (heading waves), pi for waves propagating from fore to aft (facing waves))
+         * \returns module of the rao for the axis, the frequency and the incidence
+         */
         double interpolate_phase(const size_t axis, const double Tp, double beta);
 
         std::vector<double> get_module_periods() const;
