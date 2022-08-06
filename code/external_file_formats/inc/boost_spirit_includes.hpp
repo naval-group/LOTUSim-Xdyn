b7e013f6ee0e972a8ff680caaae856c2df87abf3
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-06 21:09:49 +0200

    :hammer: Reorder includes

--- code/external_file_formats/inc/boost_spirit_includes.hpp
+++ code/external_file_formats/inc/boost_spirit_includes.hpp
@@ -9,16 +9,17 @@
 #define BOOST_SPIRIT_INCLUDES_HPP_
 
 #include <boost/config/warning_disable.hpp>
+#include <boost/foreach.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/variant.hpp>
 #include <boost/variant/recursive_variant.hpp>
-#include <boost/foreach.hpp>
-#include <boost/fusion/include/io.hpp>
 
 namespace fusion = boost::fusion;
 namespace phoenix = boost::phoenix;
