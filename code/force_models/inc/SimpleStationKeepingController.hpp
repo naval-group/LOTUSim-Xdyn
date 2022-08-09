e875cb8f8524ccd334bf17367ee9516b65a229d5
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-09 15:01:32 +0200

    :hammer: Fixed missing override

--- code/force_models/inc/SimpleStationKeepingController.hpp
+++ code/force_models/inc/SimpleStationKeepingController.hpp
@@ -28,7 +28,7 @@ class SimpleStationKeepingController : public ForceModel
         static std::string model_name();
 
         SimpleStationKeepingController(const Yaml& input, const std::string& body_name, const EnvironmentAndFrames& env);
-        Wrench get_force(const BodyStates& states, const double t, const EnvironmentAndFrames& env, const std::map<std::string,double>& commands) const;
+        Wrench get_force(const BodyStates& states, const double t, const EnvironmentAndFrames& env, const std::map<std::string,double>& commands) const override;
 
     private:
         SimpleStationKeepingController();
