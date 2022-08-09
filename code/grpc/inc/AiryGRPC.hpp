bcf762835c512fbeb2b3c786d06e240441b9c788
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-09 14:35:25 +0200

    :bug: Fixed forward declaration
    https://github.com/Gjacquenot/test_clang/runs/7745177695?check_suite_focus=true
    
    [613/908] Building CXX object grpc/CMakeFiles/grpc_airy.dir/src/AiryGRPC.cpp.o
    FAILED: grpc/CMakeFiles/grpc_airy.dir/src/AiryGRPC.cpp.o
    /usr/bin/clang++ -DGTEST_HAS_POSIX_RE=0 -I. -I/opt/share/code/grpc/inc -I/opt/share/code/ssc -I/opt/share/code/exceptions/inc -I/opt/share/code/external_data_structures/inc -I/opt/share/code/core/inc -I/opt/share/code/environment_models/inc -I/opt/share/code/hdb_interpolators/inc -I/opt/share/code/mesh/inc -isystem /opt/eigen -isystem /opt/boost/include -isystem /opt/grpc/include -isystem grpc -isystem /opt/share/code/yaml-cpp/src -isystem /opt/share/code/yaml-cpp/include -isystem /opt/share/code/yaml-cpp/include/yaml-cpp -Werror -pedantic -pedantic-errors -Wextra -Wall -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value -Wunused-variable -fno-common -Wformat=2 -Winit-self -Wpacked -Wpointer-arith -Wmissing-declarations -Wmissing-format-attribute -Wsign-compare -Wstrict-aliasing=2 -Wundef -Wconversion -Wno-misleading-indentation -Woverloaded-virtual -Weffc++ -Wwrite-strings -Wfatal-errors -Wno-deprecated -Wvariadic-macros -fPIC -std=gnu++11 -O3 -DNDEBUG -MD -MT grpc/CMakeFiles/grpc_airy.dir/src/AiryGRPC.cpp.o -MF grpc/CMakeFiles/grpc_airy.dir/src/AiryGRPC.cpp.o.d -o grpc/CMakeFiles/grpc_airy.dir/src/AiryGRPC.cpp.o -c /opt/share/code/grpc/src/AiryGRPC.cpp
    In file included from /opt/share/code/grpc/src/AiryGRPC.cpp:3:
    In file included from /opt/share/code/core/inc/EnvironmentAndFrames.hpp:11:
    /opt/share/code/core/inc/Body.hpp:23:1: fatal error: struct 'EnvironmentAndFrames' was previously declared as a class; this is valid, but may result in linker errors under the Microsoft C++ ABI [-Wmismatched-tags]
    struct EnvironmentAndFrames;
    ^
    /opt/share/code/grpc/inc/AiryGRPC.hpp:7:7: note: previous use is here
    class EnvironmentAndFrames;
          ^
    1 error generated.

--- code/grpc/inc/AiryGRPC.hpp
+++ code/grpc/inc/AiryGRPC.hpp
@@ -4,7 +4,7 @@
 #ifndef GRPC_INC_AIRYGRPC_HPP_
 #define GRPC_INC_AIRYGRPC_HPP_
 
-class EnvironmentAndFrames;
+struct EnvironmentAndFrames;
 void run_xdyn_airy_server(const EnvironmentAndFrames& env, const short unsigned int port_number);
 
 #endif /* GRPC_INC_AIRYGRPC_HPP_ */
