ab5f36e3443cf01d4484c0080a4f16da31c9f0d0
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-16 21:53:55 +0200

    :hammer: Fixed missing override keyword
    See https://github.com/Gjacquenot/test_clang/runs/7865696431?check_suite_focus=true
    
    [644/908] Building CXX object observers_and_api/CMakeFiles/observers_and_api.dir/src/ListOfObservers.cpp.o
    FAILED: observers_and_api/CMakeFiles/observers_and_api.dir/src/ListOfObservers.cpp.o
    /usr/bin/clang++ -DGTEST_HAS_POSIX_RE=0 -I. -I/opt/share/code/observers_and_api/inc -I/opt/share/code/exceptions/inc -I/opt/share/code/ssc -I/opt/share/code/interface_hdf5/inc -I/opt/share/code/external_data_structures/inc -I/opt/share/code/core/inc -I/opt/share/code/hdb_interpolators/inc -I/opt/share/code/force_models/inc -I/opt/share/code/mesh/inc -I/opt/share/code/yaml_parser/inc -I/opt/share/code/environment_models/inc -I/opt/share/code/external_file_formats/inc -I/opt/share/code/listeners_and_controllers/inc -I/opt/share/code/grpc/inc -isystem /usr/local/hdf5/include -isystem /opt/eigen -isystem /opt/eigen3-hdf5 -isystem /opt/boost/include -isystem /opt/share/code/base91x -isystem /opt/share/code/yaml-cpp/src -isystem /opt/share/code/yaml-cpp/include -isystem /opt/share/code/yaml-cpp/include/yaml-cpp -isystem /opt/grpc/include -Werror -pedantic -pedantic-errors -Wextra -Wall -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value -Wunused-variable -fno-common -Wformat=2 -Winit-self -Wpacked -Wpointer-arith -Wmissing-declarations -Wmissing-format-attribute -Wsign-compare -Wstrict-aliasing=2 -Wundef -Wconversion -Wno-misleading-indentation -Woverloaded-virtual -Weffc++ -Wwrite-strings -Wfatal-errors -Wno-deprecated -Wvariadic-macros -fPIC -std=gnu++11 -std=gnu++0x -O3 -DNDEBUG -MD -MT observers_and_api/CMakeFiles/observers_and_api.dir/src/ListOfObservers.cpp.o -MF observers_and_api/CMakeFiles/observers_and_api.dir/src/ListOfObservers.cpp.o.d -o observers_and_api/CMakeFiles/observers_and_api.dir/src/ListOfObservers.cpp.o -c /opt/share/code/observers_and_api/src/ListOfObservers.cpp
    In file included from /opt/share/code/observers_and_api/src/ListOfObservers.cpp:13:
    /opt/share/code/observers_and_api/inc/Hdf5Observer.hpp:28:14: fatal error: 'write_before_simulation' overrides a member function but is not marked 'override' [-Winconsistent-missing-override]
            void write_before_simulation(const std::vector<FlatDiscreteDirectionalWaveSpectrum>& val, const DataAddressing& address);
                 ^
    /opt/share/code/core/inc/Observer.hpp:71:22: note: overridden virtual function is here
            virtual void write_before_simulation(const std::vector<FlatDiscreteDirectionalWaveSpectrum>& val, const DataAddressing& address);
                         ^
    1 error generated.

--- code/observers_and_api/inc/Hdf5Observer.hpp
+++ code/observers_and_api/inc/Hdf5Observer.hpp
@@ -25,7 +25,7 @@ class Hdf5Observer : public Observer
         Hdf5Observer(const std::string& filename, const std::vector<std::string>& data);
         void write_before_simulation(const MeshPtr mesh, const DataAddressing& address) override;
         void write_before_simulation(const std::string& data, const DataAddressing& address) override;
-        void write_before_simulation(const std::vector<FlatDiscreteDirectionalWaveSpectrum>& val, const DataAddressing& address);
+        void write_before_simulation(const std::vector<FlatDiscreteDirectionalWaveSpectrum>& val, const DataAddressing& address) override;
         void write_command_line_before_simulation(const std::string& command_line);
         void write_yaml_before_simulation(const std::string& yaml);
         void write_matlab_script_before_simulation();
