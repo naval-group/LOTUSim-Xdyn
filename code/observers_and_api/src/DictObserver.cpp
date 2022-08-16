e3c90e9df575ea271359283e18bf05d86fe5bb65
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-08-16 22:45:27 +0200

    :hammer: Fixed size_t/Eigen::Index conversion
    See https://github.com/Gjacquenot/test_clang/runs/7866537285?check_suite_focus=true
    
    [652/908] Building CXX object observers_and_api/CMakeFiles/observers_and_api.dir/src/DictObserver.cpp.o
    FAILED: observers_and_api/CMakeFiles/observers_and_api.dir/src/DictObserver.cpp.o
    /usr/bin/clang++ -DGTEST_HAS_POSIX_RE=0 -I. -I/opt/share/code/observers_and_api/inc -I/opt/share/code/exceptions/inc -I/opt/share/code/ssc -I/opt/share/code/interface_hdf5/inc -I/opt/share/code/external_data_structures/inc -I/opt/share/code/core/inc -I/opt/share/code/hdb_interpolators/inc -I/opt/share/code/force_models/inc -I/opt/share/code/mesh/inc -I/opt/share/code/yaml_parser/inc -I/opt/share/code/environment_models/inc -I/opt/share/code/external_file_formats/inc -I/opt/share/code/listeners_and_controllers/inc -I/opt/share/code/grpc/inc -isystem /usr/local/hdf5/include -isystem /opt/eigen -isystem /opt/eigen3-hdf5 -isystem /opt/boost/include -isystem /opt/share/code/base91x -isystem /opt/share/code/yaml-cpp/src -isystem /opt/share/code/yaml-cpp/include -isystem /opt/share/code/yaml-cpp/include/yaml-cpp -isystem /opt/grpc/include -Werror -pedantic -pedantic-errors -Wextra -Wall -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value -Wunused-variable -fno-common -Wformat=2 -Winit-self -Wpacked -Wpointer-arith -Wmissing-declarations -Wmissing-format-attribute -Wsign-compare -Wstrict-aliasing=2 -Wundef -Wconversion -Wno-misleading-indentation -Woverloaded-virtual -Weffc++ -Wwrite-strings -Wfatal-errors -Wno-deprecated -Wvariadic-macros -fPIC -std=gnu++11 -std=gnu++0x -O3 -DNDEBUG -MD -MT observers_and_api/CMakeFiles/observers_and_api.dir/src/DictObserver.cpp.o -MF observers_and_api/CMakeFiles/observers_and_api.dir/src/DictObserver.cpp.o.d -o observers_and_api/CMakeFiles/observers_and_api.dir/src/DictObserver.cpp.o -c /opt/share/code/observers_and_api/src/DictObserver.cpp
    /opt/share/code/observers_and_api/src/DictObserver.cpp:70:35: fatal error: implicit conversion changes signedness: 'unsigned long' to 'Eigen::Index' (aka 'long') [-Wsign-conversion]
            const double xmax = s.x[nx-1];
                                ~   ~~^~
    1 error generated.

--- code/observers_and_api/src/DictObserver.cpp
+++ code/observers_and_api/src/DictObserver.cpp
@@ -63,16 +63,16 @@ std::function<void()> DictObserver::get_initializer(const double, const DataAddr
 std::function<void()> DictObserver::get_serializer(const SurfaceElevationGrid& s, const DataAddressing&)
 {
     return [this,s](){
-        const size_t nx = (size_t)s.x.size();
-        const size_t ny = (size_t)s.y.size();
-        const size_t n = (size_t)s.z.size();
+        const Eigen::Index nx = s.x.size();
+        const Eigen::Index ny = s.y.size();
+        const Eigen::Index n = s.z.size();
         const double xmin = s.x[0];
         const double xmax = s.x[nx-1];
         const double ymin = s.y[0];
         const double ymax = s.y[ny-1];
-        std::vector<float> v(n,0.0);
+        std::vector<float> v(static_cast<size_t>(n), 0.0);
         double const * const data = s.z.data();
-        for (size_t i=0;i<n;++i) v[i] = (float)data[i];
+        for (Eigen::Index i=0;i<n;++i) v[static_cast<size_t>(i)] = (float)data[i];
         ssSurfaceElevationGrid.str(std::string());
         if (n>0) ssSurfaceElevationGrid << "\"waves\":{\"nx\":" << nx <<",\"ny\":" << ny
                                         << ",\"xmin\":"<< xmin << ",\"xmax\":"<< xmax
