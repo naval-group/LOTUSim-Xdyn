b8f38b37c827a80936da11b0e93c845024b453b8
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-08-23 14:31:24 +0200

    :hammer: Merged src/inc files from binary_stl_data

--- code/binary_stl_data/generate_test_ship.hpp
+++ code/binary_stl_data/generate_test_ship.hpp
@@ -1,15 +1,14 @@
 /*
- * generate_anthineas.hpp
+ * generate_test_ship.hpp
  *
  *  Created on: Aug 26, 2014
  *      Author: cady
  */
-
-#ifndef GENERATE_ANTHINEAS_HPP_
-#define GENERATE_ANTHINEAS_HPP_
+#ifndef GENERATE_TEST_SHIP_HPP_
+#define GENERATE_TEST_SHIP_HPP_
 
 #include "GeometricTypes3d.hpp"
 
 VectorOfVectorOfPoints test_ship();
 
-#endif  /* GENERATE_ANTHINEAS_HPP_ */
+#endif
