ae1948e77e98e8451590b2d08cd955e4db2cec92
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-08-24 12:01:39 +0200

    :pencil: Updated doc path due to directories renaming

--- code/core/Res.hpp
+++ code/core/Res.hpp
@@ -18,9 +18,9 @@
  *  \details This structure will be outputted at each simulation step.
  *  \ingroup simulator
  *  \section ex1 Example
- *  \snippet simulator/unit_tests/src/ResTest.cpp ResTest example
+ *  \snippet simulator/unit_tests/ResTest.cpp ResTest example
  *  \section ex2 Expected output
- *  \snippet simulator/unit_tests/src/ResTest.cpp ResTest expected output
+ *  \snippet simulator/unit_tests/ResTest.cpp ResTest expected output
  */
 struct Res
 {
