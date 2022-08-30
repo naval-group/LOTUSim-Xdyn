27d913a2339eb2636d1476a8fe8952b8ca913f3e
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-08-30 18:13:09 +0200

    :pencil: Typo udpate

--- code/core/WaveModelBuilder.hpp
+++ code/core/WaveModelBuilder.hpp
@@ -26,17 +26,18 @@ struct DiscreteDirectionalWaveSpectrum;
 class WaveModelBuilderInterface
 {
     public:
-        WaveModelBuilderInterface(const TR1(shared_ptr)<std::vector<DirectionalSpreadingBuilderPtr> >& directional_spreading_parsers_,
-                                  const TR1(shared_ptr)<std::vector<SpectrumBuilderPtr> >& spectrum_parsers_) : directional_spreading_parsers(directional_spreading_parsers_),
-                                  spectrum_parsers(spectrum_parsers_)
-                                  {}
+        WaveModelBuilderInterface(
+            const TR1(shared_ptr)<std::vector<DirectionalSpreadingBuilderPtr> >& directional_spreading_parsers_,
+            const TR1(shared_ptr)<std::vector<SpectrumBuilderPtr> >& spectrum_parsers_) :
+                directional_spreading_parsers(directional_spreading_parsers_),
+                spectrum_parsers(spectrum_parsers_)
+                {}
         virtual ~WaveModelBuilderInterface() {}
         virtual boost::optional<TR1(shared_ptr)<WaveModel> > try_to_parse(const std::string& model, const DiscreteDirectionalWaveSpectrum& spectrum, const std::string& yaml) const = 0;
 
     protected:
         TR1(shared_ptr)<std::vector<DirectionalSpreadingBuilderPtr> > directional_spreading_parsers;
         TR1(shared_ptr)<std::vector<SpectrumBuilderPtr> > spectrum_parsers;
-
 };
 
 template <typename T> class WaveModelBuilder : public WaveModelBuilderInterface
