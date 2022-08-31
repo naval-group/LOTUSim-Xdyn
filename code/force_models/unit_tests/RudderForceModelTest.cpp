01a949556fa78a7acd24700e707888b8aca18d19
ANCEAU Antoine <antoine.anceau@sirehna.com>
2022-08-31 08:49:50 +0000

    :ok_hand: Removed commented lines (AAN's suggestion) in code/force_models/unit_tests/RudderForceModelTest.cpp
    See https://gitlab.sirehna.com/sirehna/xdyn/-/merge_requests/186#note_129052

--- code/force_models/unit_tests/RudderForceModelTest.cpp
+++ code/force_models/unit_tests/RudderForceModelTest.cpp
@@ -145,9 +145,6 @@ TEST_F(RudderForceModelTest, get_wrench)
     parameters.b = 99;
     parameters.effective_aspect_ratio_factor = 2.3;
     RudderForceModel::RudderModel riw(parameters,1024,0.75);
-    // const double drag = 10;
-    // const double lift = 200;
-    // const double angle = -PI/3;
     const double area = 1.467;
     ssc::kinematics::Vector6d v = riw.get_wrench(3,4,0.5,area);
     ASSERT_DOUBLE_EQ(-2021.4412785509464, v(0));
