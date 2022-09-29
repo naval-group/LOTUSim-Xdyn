6126bee013d17931d4cf2b8889ed88db017404dd
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2022-09-29 14:26:53 +0200

    :pencil: Snippet update

--- code/xdyn/core/BodyBuilder.hpp
+++ code/xdyn/core/BodyBuilder.hpp
@@ -20,9 +20,9 @@ struct YamlRotation;
  *  \brief Builds a Body object from the YAML & STL describing it
  *  \ingroup simulator
  *  \section ex1 Example
- *  \snippet simulator/unit_tests/BodyBuilderTest.cpp BodyBuilderTest example
+ *  \snippet core/unit_tests/BodyBuilderTest.cpp BodyBuilderTest example
  *  \section ex2 Expected output
- *  \snippet simulator/unit_tests/BodyBuilderTest.cpp BodyBuilderTest expected output
+ *  \snippet core/unit_tests/BodyBuilderTest.cpp BodyBuilderTest expected output
  */
 class BodyBuilder
 {
